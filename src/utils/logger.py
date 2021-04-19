163ffeb4930a4da569f87e8801be9932c0c99d8c
lfischl <lorenz.fischl@cern.ch>
2021-04-19 11:43:10 +0200

    minore changes to enhance utilities

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,20 +5,15 @@ from sys import stderr
 def logger(level_console_handler: str = "INFO", level_file_handler: str ="DEBUG", logger_name: str = "log"):
     """
     This function returns a custom logger. It writes log outputs in the console and
-    a log file and can even write them to Telegram. The format is custom. The warning level is defined by the
-    input strings. use:
-    log = make_loggger("INFO", "DEBUG", "DEBUG")
+    a log file. The format is custom. The warning level is defined by the input strings. use ex:
+    log = make_loggger("INFO", "DEBUG")
     log.warning("This is a warning")  #  a warning will be written in the console and a more detailed on in the log file
-    :param level_console_handler: string, has to be contained in (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-    :param level_file_handler: string, has to be contained in (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-    :param level_telegram_handler: string, has to be contained in (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    :param level_console_handler: Defines the level from which log messages will be displayed in the console. Has to be contained in (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    :param level_file_handler: Defines the level from which log messages will be displayed in the console. Has to be contained in (DEBUG, INFO, WARNING, ERROR, CRITICAL)
     """
-    if level_console_handler not in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] or \
-            level_file_handler not in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", "", None]:
-        raise Exception("The inpus string has to be one of (DEBUG, INFO, WARNING, ERROR, CRITICAL)")
 
     log = logging.getLogger(logger_name)  # create logger
-    log.setLevel("DEBUG")
+    log.setLevel("DEBUG")  # when a log file will be created (and next handled by the log handlers) (like a lower boundary)
     if log.hasHandlers():
         print("logger named ""API_logger"" already has handler. Ddeleting old hanlders and adding new ones")
         for handler in log.handlers:
@@ -36,22 +31,23 @@ def logger(level_console_handler: str = "INFO", level_file_handler: str ="DEBUG"
         log.addHandler(fh)  # add this hanlder to the logger
 
     # CONSOLE log handler: writes log files to the python console
-    format_string = "%(levelname)-5s[line%(lineno)3s|%(funcName)-15s]:%(message)s"  # add logger name if differnet loggers exist
-    formatter_ch = logging.Formatter(format_string)  # create formatter
-    ch = logging.StreamHandler(stderr)  # create console handler (output in the console)
-    ch.setLevel(level_console_handler)  # set output lvl (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-    ch.setFormatter(formatter_ch)  # setting the custom format to the console handler
-    log.addHandler(ch)  # add this handler to the logger
+    if level_console_handler!= None:
+        format_string = "%(levelname)-5s[line%(lineno)3s|%(funcName)-15s]:%(message)s"  # add logger name if differnet loggers exist
+        formatter_ch = logging.Formatter(format_string)  # create formatter
+        ch = logging.StreamHandler(stderr)  # create console handler (output in the console)
+        ch.setLevel(level_console_handler)  # set output lvl (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+        ch.setFormatter(formatter_ch)  # setting the custom format to the console handler
+        log.addHandler(ch)  # add this handler to the logger
 
     return log
 
-def log_w_tg(log: logging.Logger, level_telegram_handler: str = "") -> None:
+def logger_add_tg(log: logging.Logger, level_telegram_handler: str = None) -> None:
     """
     This will add a log handler to log that will write logs to telegram.
     You need to have a telegram bot setup with access informatino located in the right folder.
     """
     import telegram_handler  # telegram log handler
-    if level_telegram_handler != "":
+    if level_telegram_handler != None:
         format_string = "%(levelname)-5s[line%(lineno)3s|%(funcName)-15s]:\n<b>%(message)s</b>"
         telegram_access_data_filepath = "/home/lfischl/.config/telegram_bot/logC_bot.txt"
         with open(telegram_access_data_filepath,
--- src/utils/memory_usage.py
+++ src/utils/memory_usage.py
@@ -1,8 +1,12 @@
 import psutil
 import os
+import logging
 
-def memo_usage():
+def memo_usage(log: logging.Logger = None):
     pid = os.getpid()
     memoryUse = psutil.Process(pid).memory_info()[0] / 2. ** 30
-    print("you are currently using " + memoryUse + "GByte of Memory")
+    if log == None:
+        print("you are currently using " + memoryUse + "GByte of Memory")
+    else:
+        log.debug("you are currently using " + memoryUse + "GByte of Memory")
 
