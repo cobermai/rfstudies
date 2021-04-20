a74929289c087bfc09a553c406b94c04b4868f86
lfischl <lorenz.fischl@cern.ch>
2021-04-20 16:18:05 +0200

    main in the API calles the XBox_class.py for transforming tdms files into pd.DataFrames and dictionaries

--- src/API/main.py
+++ src/API/main.py
@@ -1,15 +1,9 @@
 from src.API.XBox_class import *
-SHOW_PROGRESS_BAR = "tqdm"
-from time import time
-t0 = time()
-guesses = [pre + "CLIC_data_transfert/CLIC_DATA_Xbox2_T24PSI_2/" for pre in ["/home/lfischl/cernbox/", "/home/thomas/cernboxML/private/", "?Christoph/","?Andrea/"]] + ["/eos/project-m/ml-for-alarm-system/private/CLIC_data_transfert/"]
-for possible_dir in guesses:
-    if os.path.isdir(possible_dir):
-        dir_path = possible_dir
+import os
 
 settings = {
-    "dir_path": "/home/lfischl/project_data/CLIC_DATA_Xbox2_T24PSI_2/" ,  # dir_path,
-    "dest_path": "/home/lfischl/output_files/",  # if not set this will be defined later as xbox3/data
+    "dir_path": os.path.expanduser("~/project_data/CLIC_DATA_Xbox2_T24PSI_2/"),
+    "dest_path": os.path.expanduser("~/output_files/"),
     "ed_ch_of_interest": ['DC Down',
                           'DC Up',
                           'PEI Amplitude',
@@ -60,11 +54,6 @@ settings = {
                           'Pulse Count'],
     "number_of_workers_for_paralell_computation": 1
 }
-#tdmsfile = nptdms.TdmsFile.read("/home/lfischl/cernbox/CLIC_data_transfert/CLIC_DATA_Xbox2_T24PSI_2/TrendData_20180328.tdms")
+
 xb2ds = XBox2DataSet(settings)
 xb2ds.transform(range(2))
-
-    #log.debug("For Day " + data.timestamp + " API took " + str(time.time() - t00) + " sek")  # or use %timeit
-
-print("in total took (sek) " + str(time() - t0))  # or use %timeit
-
