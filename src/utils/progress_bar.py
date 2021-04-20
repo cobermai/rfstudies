b4cebdf204302b5afca4675c5a9b58d175e11135
lfischl <lorenz.fischl@cern.ch>
2021-04-20 09:06:23 +0200

    Added more comments for explanantion

--- src/utils/progress_bar.py
+++ src/utils/progress_bar.py
@@ -1,15 +1,23 @@
 from tqdm import tqdm
-from tg_tqdm import tg_tqdm
+import os
 
-if "SHOW_PROGRESS_BAR" not in locals(): SHOW_PROGRESS_BAR = ""
+# This is a trick to ensure global compability. If the global variable SHOW_PROGRESS_BAR has
+# not been set yet, it gets set to ""
+if "SHOW_PROGRESS_BAR" not in locals(): SHOW_PROGRESS_BAR = None
 
 def get_bar(iter_obj):
+    """
+    Ads a visible progress bar (tqdm) in either the command line or on the TG messenger. The global variable
+    SHOW_PROGRESS_BAR defines the settings
+    :param iter_obj: any iteration object in a for loop
+    """
     if SHOW_PROGRESS_BAR == "tqdm":
         return tqdm(iter_obj)
     elif SHOW_PROGRESS_BAR == "tg_tqdm":
         try:
-            with open("/home/lfischl/.config/telegram_bot/tg_tqdm.txt") as file:
-                return tg_tqdm(iter_obj, file.readline()[:-1], file.readline()[:-1])
+            path = os.path.expanduser("~/.config/telegram_bot/tg_tqdm.txt")
+            with open(path) as file:
+                return tqdm(iter_obj, token = file.readline()[:-1], chat_id = file.readline()[:-1])
         except FileNotFoundError as fnf_err:
             print("found no access information --> Showing Telegram progess bar is not possible")
             print(fnf_err)
@@ -17,7 +25,7 @@ def get_bar(iter_obj):
     else:
         return iter_obj
 
-"""  here is an older Idea of how to show progress
+"""  Older version to show progress. This has to be put inside the loop
 import sys
 sys.stdout.write("\r progress: %d%%" % (progress_num))
 sys.stdout.flush()
