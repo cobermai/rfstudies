e97c17014861d95ec4ff588db4f38aa2bc135d94
lfischl <lorenz.fischl@cern.ch>
2021-04-20 12:34:06 +0200

    logging to a file is now more robust. the log file folder is now always located next to the logger.py file Added use of the logger name. It now is contianed in the format string

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,8 +1,9 @@
 import logging
 from datetime import datetime
 from sys import stderr
+import os
 
-def logger(level_console_handler: str = "INFO", level_file_handler: str ="DEBUG", logger_name: str = "log"):
+def logger(level_console_handler: str = "INFO", level_file_handler: str ="DEBUG", name: str = "MLOG"):
     """
     This function returns a custom logger. It writes log outputs in the console and
     a log file. The format is custom. The warning level is defined by the input strings. use ex:
@@ -10,36 +11,40 @@ def logger(level_console_handler: str = "INFO", level_file_handler: str ="DEBUG"
     log.warning("This is a warning")  #  a warning will be written in the console and a more detailed on in the log file
     :param level_console_handler: Defines the level from which log messages will be displayed in the console. Has to be contained in (DEBUG, INFO, WARNING, ERROR, CRITICAL)
     :param level_file_handler: Defines the level from which log messages will be displayed in the console. Has to be contained in (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-    """
+    :param name: the name of the logger. ML + LOG = MLOG """
 
-    log = logging.getLogger(logger_name)  # create logger
-    log.setLevel("DEBUG")  # when a log file will be created (and next handled by the log handlers) (like a lower boundary)
+    log = logging.getLogger(name)  # create logger
     if log.hasHandlers():
-        print("logger named ""API_logger"" already has handler. Ddeleting old hanlders and adding new ones")
-        for handler in log.handlers:
-            log.removeHandler(handler)
+        log.makeRecord(name= "init_logger_msg", level=log.getEffectiveLevel(),
+                       msg= "Info: logger initializer was called and MLOG already existed")
+    else:
+        log.setLevel("DEBUG")  # when a log file will be created (and next handled by the log handlers) (like a lower boundary)
+        # FILE log handler: writes log files to a txt file
+        if level_file_handler != None:
+            format_string = "%(name)-10s|%(asctime)s|%(levelname)-8s|%(filename)-20s|line%(lineno)3s|%(funcName)-15s|%(message)s"
+            formatter_fh = logging.Formatter(format_string)  # create formatter
+            date = str(datetime.date(datetime.today()))
+            dir_path = os.path.dirname(__file__) + "/log_files/"
+            file_name = "log_file_" + date + ".txt"
+            if not os.path.isdir(dir_path): os.mkdir(dir_path)
+            if not os.path.isfile(dir_path + file_name):
+                with open(dir_path + file_name, "w") as f:
+                    f.write("name      |date                   |level   |filename            |line_no|function_name  |message\n")
+            fh = logging.FileHandler(dir_path + file_name) # create file handler (written to log file)
+            fh.setLevel(level_file_handler)  # set output lvl (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+            fh.setFormatter(formatter_fh)  # setting the custom format to the console handler
+            log.addHandler(fh)  # add this hanlder to the logger
 
-    # FILE log handler: writes log files to a txt file
-    if level_file_handler != None:
-        format_string = "%(asctime)s|%(levelname)-8s|%(filename)-20s|line%(lineno)3s|%(funcName)-15s|%(message)s"
-        formatter_fh = logging.Formatter(format_string)  # create formatter
-        date = str(datetime.date(datetime.today()))
-        filepath_logfile = "/log_file_"
-        fh = logging.FileHandler(filepath_logfile + date + ".txt") # create file handler (written to log file)
-        fh.setLevel(level_file_handler)  # set output lvl (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-        fh.setFormatter(formatter_fh)  # setting the custom format to the console handler
-        log.addHandler(fh)  # add this hanlder to the logger
+        # CONSOLE log handler: writes log files to the python console
+        if level_console_handler!= None:
+            format_string = "%(name)s: %(levelname)-5s[line%(lineno)3s|%(funcName)-15s]:%(message)s"  # add logger name if differnet loggers exist
+            formatter_ch = logging.Formatter(format_string)  # create formatter
+            ch = logging.StreamHandler(stderr)  # create console handler (output in the console)
+            ch.setLevel(level_console_handler)  # set output lvl (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+            ch.setFormatter(formatter_ch)  # setting the custom format to the console handler
+            log.addHandler(ch)  # add this handler to the logger
 
-    # CONSOLE log handler: writes log files to the python console
-    if level_console_handler!= None:
-        format_string = "%(levelname)-5s[line%(lineno)3s|%(funcName)-15s]:%(message)s"  # add logger name if differnet loggers exist
-        formatter_ch = logging.Formatter(format_string)  # create formatter
-        ch = logging.StreamHandler(stderr)  # create console handler (output in the console)
-        ch.setLevel(level_console_handler)  # set output lvl (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-        ch.setFormatter(formatter_ch)  # setting the custom format to the console handler
-        log.addHandler(ch)  # add this handler to the logger
-
-    return log
+        return log
 
 def logger_add_tg(log: logging.Logger, level_telegram_handler: str = None) -> None:
     """
@@ -49,9 +54,8 @@ def logger_add_tg(log: logging.Logger, level_telegram_handler: str = None) -> No
     import telegram_handler  # telegram log handler
     if level_telegram_handler != None:
         format_string = "%(levelname)-5s[line%(lineno)3s|%(funcName)-15s]:\n<b>%(message)s</b>"
-        telegram_access_data_filepath = "/home/lfischl/.config/telegram_bot/logC_bot.txt"
-        with open(telegram_access_data_filepath,
-                  "r") as file:
+        path = os.path.expanduser("~/.config/telegram_bot/tg_tqdm.txt")
+        with open(path) as file:
             formatter_tg = telegram_handler.HtmlFormatter(format_string)
             tgh = telegram_handler.TelegramHandler(token=file.readline()[:-1],
                                                    chat_id=file.readline()[:-1],
