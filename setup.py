0c209644c8f1abf4630b74c4f4c31319fb768780
Lorenz Fischl <lorenz.fischl@cern.ch>
2021-05-19 07:40:58 +0000

    first version of the setup.py file

--- setup.py
+++ setup.py
@@ -1,28 +1,23 @@
-"""
-Setup file. Install the mlframework with `pip install mlframework`
-"""
 from setuptools import setup, find_packages
 
 REQUIREMENTS: dict = {
     'core': [
-        'tqdm >= 4.60.0',
-        'requests >= 2.25.1',
-        'psutil >= 5.8.0',
-        'npTDMS >= 1.2.0',
-        'h5py >= 3.2.1',
-        'matplotlib >= 3.4.1',
-        'compress_pickle >= 2.0.1',
-        'scipy >= 1.6.3',
-        'pyyaml>=5.4',
-        'pandas>=1.2.4',
-        'coloredlogs>=15',
+        'numpy',
+        'pandas',
+        'datetime',
+        'matplotlib',
+        "nptdms",
+        "h5py",
+        "psutil",
+        "tqdm",
     ],
     'test': [
-        'pytest >= 6.2.4',
+        'pytest',
     ],
     'dev': [
+        "python-telegram-handler",
     ],
-    'doc': [  # sphinx (?)
+    'doc': [  # sphinx
     ],
 }
 
@@ -41,10 +36,16 @@ setup(
     install_requires=REQUIREMENTS['core'],
     extras_require={
         **REQUIREMENTS,
+        # The 'dev' extra is the union of 'test' and 'doc', with an option
+        # to have explicit development dependencies listed.
         'dev': [req
                 for extra in ['dev', 'test', 'doc']
                 for req in REQUIREMENTS.get(extra, [])],
+        # The 'all' extra is the union of all requirements.
         'all': [req for reqs in REQUIREMENTS.values() for req in reqs],
     },
     packages=find_packages(),
+    entry_points={
+        'console_scripts': ['mlframework=src:main'],
+    },
 )
