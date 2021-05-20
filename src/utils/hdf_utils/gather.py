6edfc72a4966eab1ebdb594b8db4b5ee5a14d2d1
lfischl <lorenz.fischl@cern.ch>
2021-05-20 19:00:08 +0200

    discarted set_num_processes, because its unnecessary changed functions from hidden to public (in order to test them, they have to be public, because I call them from somewhere else) and added descriptions

--- src/utils/hdf_utils/gather.py
+++ src/utils/hdf_utils/gather.py
@@ -4,7 +4,6 @@ Currently implemented:
 * Gather hdf groups of arbitrary layer scattered on many hdf files with external links (hdf external links).
   This gathers groups without copying its contents.
 """
-import os
 from typing import Callable
 import logging
 from pathlib import Path
@@ -31,39 +30,55 @@ def hdf_path_combine(*argv: str) -> str:
     return rem_double_dash("/" + "/".join(argv))
 
 
-def _get_ext_link_rek(file_path: Path,
-                      hdf_path: str,
-                      depth_to_go: int,
-                      func_to_fulfill: Callable[[Path, str], bool]) -> set:
+def get_ext_link_rec(file_path: Path,
+                     hdf_path: str,
+                     depth_to_go: int,
+                     func_to_fulfill: Callable[[Path, str], bool]) -> set:
+    """
+    recursive function to return external links of hdf files in variable depth
+    :param file_path: path of the hdf file
+    :param hdf_path: hdf_path of the hdf object inside the hdf file
+    :param depth_to_go: goal depth to go
+    :param func_to_fulfill: the function that has to be fulfilled in order to be gathered
+    :return: set of external hdf links
+    """
     if depth_to_go==0:
         ret = {h5py.ExternalLink(file_path, hdf_path)} if func_to_fulfill(file_path, hdf_path) else set()
     elif depth_to_go>0:
         with h5py.File(file_path, "r") as file:
             ret_set = set({})
             for key in file[hdf_path].keys():
-                ret_set.update(set(_get_ext_link_rek(file_path=file_path,
-                                                     hdf_path=hdf_path_combine(hdf_path, key),
-                                                     depth_to_go=depth_to_go - 1,
-                                                     func_to_fulfill= func_to_fulfill)))
+                ret_set.update(set(get_ext_link_rec(file_path=file_path,
+                                                    hdf_path=hdf_path_combine(hdf_path, key),
+                                                    depth_to_go=depth_to_go - 1,
+                                                    func_to_fulfill= func_to_fulfill)))
             ret = ret_set
     else:
         raise ValueError("depth_to_go should be a non negative integer")
     return ret
 
-def _hdf_write_ext_links(from_file_path: Path,
+def hdf_write_ext_links(source_file_path: Path,
                         dest_file_path: Path,
                         lock: Lock_Data_Type,
                         depth: int,
                         func_to_fulfill: Callable[[Path, str], bool]) -> None:
-    ext_link_list = _get_ext_link_rek(file_path=from_file_path,
-                                      hdf_path="/",
-                                      depth_to_go=depth,
-                                      func_to_fulfill=func_to_fulfill)
+    """
+    writes external links from the source file of given depth into the destination file.
+    :param source_file_path: file path of the hdf file
+    :param dest_file_path: file path of the destination hdf file
+    :param lock: lock to gain writing access to the dest hdf file
+    :param depth: the depth in which external links will be created
+    :param func_to_fulfill: tthe function that has to be fulfilled in order to be gathered
+    """
+    ext_link_list = get_ext_link_rec(file_path=source_file_path,
+                                     hdf_path="/",
+                                     depth_to_go=depth,
+                                     func_to_fulfill=func_to_fulfill)
     with lock:
         with h5py.File(dest_file_path, "a") as dest_file:
             rek_grp_name = lambda x: x + "(new)" if dest_file.get(x, None) is not None else x.replace("/", "-")
             for link in ext_link_list:
-                grp_name = rek_grp_name(from_file_path.stem + ":" + link.path[1:] if link.path[0]=="/" else link.path )
+                grp_name = rek_grp_name(source_file_path.stem + "-" + link.path)
                 dest_file[grp_name] = link
             dest_file.flush()
 
@@ -72,20 +87,16 @@ class Gather:
     """
     combines hdf groups of many hdf files into one by creating external links that point to the original files
     """
-    def __init__(self, depth: int = 1, num_processes: int = None):
+    def __init__(self, depth: int = 1, num_processes: int = 2):
         '''
         :param depth: the depth of the hdf groups to combine
         :param num_processes: number of processing kernels, None -> os.cpu_count
         '''
         self.depth: int = depth
-        self.set_num_processes(num_processes)
+        self.num_processes = num_processes
         self.dest_file_path: Path
         self.source_file_paths: Iterable
         self.func_to_fulfill: Callable[[Path, str], bool]
-
-    def set_num_processes(self, num_processes: int = None) -> None:
-        """Sets the num_processes for the ThreadPool, if None is given its set to the number of logical cpu cores"""
-        self.num_processes: int = os.cpu_count() or 1 if num_processes is None else num_processes
 
     def set_func_to_fulfill(self, on_error: bool, func_to_fulfill: Callable[[Path, str], bool] = None) -> None:
         """Sets the function_to_fulfill. On True the HdfObject will be added, if False it will not be added.
@@ -123,7 +134,7 @@ class Gather:
         self.dest_file_path = dest_file_path
         return self
 
-    def if_fulfills(self, func_to_fulfill: Callable[[Path, str], bool] = None, on_error: bool = False) -> None:
+    def if_fulfills(self, func_to_fulfill: Callable[[Path, str], bool] = None, on_error: bool = False):
         """
         if func_to_fulfill is fulfilled the link to the hdf_object will be added
         :param func_to_fulfill: function to be fulfilled by a feasible hdf_object
@@ -134,11 +145,11 @@ class Gather:
 
     def run_with_external_links(self) -> None:
         """This starts the Gathering by creating hdf ExternalLink objects"""
-        multi_proc_func = partial(_hdf_write_ext_links,
-                                    dest_file_path=self.dest_file_path,
-                                    lock=Lock(),
-                                    depth=self.depth,
-                                    func_to_fulfill=self.func_to_fulfill)
+        multi_proc_func = partial(hdf_write_ext_links,
+                                  dest_file_path=self.dest_file_path,
+                                  lock=Lock(),
+                                  depth=self.depth,
+                                  func_to_fulfill=self.func_to_fulfill)
         with ThreadPool(self.num_processes) as pool:
             pool.map(multi_proc_func, self.source_file_paths)
         log.debug("finished Gathering for %s", self.dest_file_path)
