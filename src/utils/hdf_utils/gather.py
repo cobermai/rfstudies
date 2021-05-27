4be8a0e94189fcd5f221eb41e0768086cc742c0f
lfischl <lorenz.fischl@cern.ch>
2021-05-27 12:03:32 +0200

    minore fixes added .close() in tdms_read for better hdf file handling and removed two comments added a comment in gather

--- src/utils/hdf_utils/gather.py
+++ src/utils/hdf_utils/gather.py
@@ -77,9 +77,7 @@ def hdf_write_ext_links(source_file_path: Path,
     with lock:
         with h5py.File(dest_file_path, "a") as dest_file:
             for link in ext_link_list:
-                #rek_grp_name = lambda x: x if dest_file.get(x, None) is None else rek_grp_name(x + "(new)")
                 grp_name = source_file_path.stem + "-" + link.path.replace("/","-")
-                #grp_name = rek_grp_name(source_file_path.stem + "-" + link.path.replace("/","-"))
                 dest_file[grp_name] = link
             dest_file.flush()
 
@@ -131,7 +129,7 @@ class Gather:
         :param dest_file_paths: Path of the destination file path
         :return: The Gather object
         """
-        h5py.File(dest_file_path, "w")  # overwrite old file
+        h5py.File(dest_file_path, "w").close()  # overwrite old file
         self.dest_file_path = dest_file_path
         return self
 
