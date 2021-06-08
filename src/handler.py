4274e75655ac7931113943cf61a0b845a1efa0c2
lfischl <lorenz.fischl@cern.ch>
2021-06-08 22:59:57 +0200

    moved the union code to a separate file. Its much more organized now.

--- src/handler.py
+++ src/handler.py
@@ -10,58 +10,14 @@ import h5py
 from pathlib import Path
 from src.utils.transf_tools.gather import hdf_path_combine
 from setup_logging import setup_logging
-setup_logging()
-LOG = logging.getLogger("test_handler")
 import numpy.typing as npt
 import dateutil.parser
 import time
+from utils.handler_tools.union import union, clean, sort_by
 
+setup_logging()
+LOG = logging.getLogger("test_handler")
 
-def union(source_file_path: Path, destination_file_path: Path):
-    """
-    for each dataset_name it unites (concatenates) all hdf-dataset at given depth to one large hdf-dataset in the
-    destination file
-    :param source_file_path:
-    :param destination_file_path:
-    :param depth:
-    :return:
-    """
-    with h5py.File(source_file_path, mode="r") as source_file, \
-         h5py.File(destination_file_path, mode="a") as dest_file:
-        dataset_name = "Timestamp"
-        data = [ts.replace("Z", "") for path in source_file.keys()
-                for ts in source_file[path][dataset_name][:].astype(str)]
-        data = np.array(data, dtype=np.datetime64)
-        dest_file.create_dataset(name=dataset_name, data=data.astype(dtype=h5py.opaque_dtype(data.dtype)),
-                                 chunks=True)
-
-        for dataset_name in ['BLM TIA Q', 'BLM TIA min', 'BLM min', 'Bunker WG Temp', 'Chiller 1', 'Chiller 2',
-                             'Chiller 3', 'Collector', 'DC Down min', 'DC Up min', 'Gun', 'IP Load', 'IP before PC',
-                             'IP before structure', 'Klystron Flange Temp', 'Load Temp', 'Loadside win', 'PC IP',
-                             'PC Left Cavity Temp', 'PC Right Cavity Temp', 'PEI FT avg', 'PEI max', 'PKI FT avg',
-                             'PKI max', 'PSI FT avg', 'PSI Pulse Width', 'PSI max', 'PSR FT avg', 'PSR max',
-                             'Pulse Count', 'Structure Input Temp', 'Tubeside win', 'US Beam Axis IP', 'WG IP']:
-            data = np.concatenate([source_file[path][dataset_name][:] for path in source_file.keys()])
-            dest_file.create_dataset(name=dataset_name, dtype=float, data=data, chunks=True)
-
-def clean(file_path: Path):
-    with h5py.File(file_path, "r+") as file:
-        key_list = [key for key in file.keys() if np.issubdtype(file[key].dtype, np.number)]
-        length = file[key_list[0]][:].shape[0]
-        is_corrupt = np.zeros((length,), dtype=bool)
-        for key in [key for key in file.keys() if np.issubdtype(file[key].dtype, np.number)]:
-            is_corrupt |= np.isnan(file[key][:]) | np.isinf(file[key][:])
-        size = sum(~is_corrupt)
-        for key in file.keys():
-            data = file[key][~is_corrupt]
-            file[key].resize(size=(size,))
-            file[key][...] = data
-
-def sort_by_timestamp(file_path: Path):
-    with h5py.File(file_path, "r+") as file:
-        indices_order = file["Timestamp"][:].argsort()
-        for key in file.keys():
-            file[key][...] = file[key][indices_order]
 
 def create_context_data(source_file_path: Path, dest_file_path: Path):
     with h5py.File(source_file_path, mode="r") as source_file, \
@@ -77,7 +33,7 @@ def create_context_data(source_file_path: Path, dest_file_path: Path):
                     'PSR Phase', 'PSR log']:
             dest_file.create_group(name=chn)
 
-        ds_timestamp= dest_file.create_dataset("Timestamp", shape=(length,), dtype=h5py.opaque_dtype("<M8[us]"), chunks=True)
+        ds_timestamp = dest_file.create_dataset("Timestamp", shape=(length,), dtype=h5py.opaque_dtype("<M8[us]"), chunks=True)
 
         label_name_dict = {0:'is_log', 1:'is_in40ms', 2:'is_in20ms', 3:'is_bd'}
         for name in label_name_dict.values():
@@ -122,17 +78,13 @@ def add_context_data():
 
 if __name__ == "__main__":
     source_file_path = Path("~/output_files/TrendDataExtLinks.hdf").expanduser()
-    destination_file_path = Path("~/output_files/combined_td.hdf").expanduser()
+    dest_file_path = Path("~/output_files/combined_td.hdf").expanduser()
 
-    if False:
-        h5py.File(destination_file_path, mode="w").close()
-        union(source_file_path=source_file_path,
-              destination_file_path=destination_file_path)
-        LOG.debug("## Union done")
-        clean(destination_file_path)
-        LOG.debug("## Clean done")
-        sort_by_timestamp(destination_file_path)
-        LOG.debug("##sort_by_timestmap done")
+    if True:
+        h5py.File(dest_file_path, mode="w").close()
+        union(source_file_path=source_file_path, dest_file_path=dest_file_path)
+        clean(file_path=dest_file_path)
+        sort_by(file_path=dest_file_path, sort_by_name="Timestamp")
     else:
         ed_links_file_path = Path("~/output_files/EventDataExtLinks.hdf").expanduser()
         destination_file_path= Path("~/output_files/context_data.hdf").expanduser()
