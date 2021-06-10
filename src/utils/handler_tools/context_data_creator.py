d80cf4c8ef38a7760a8c3a0460701390d6077e0a
lfischl <lorenz.fischl@cern.ch>
2021-06-10 16:07:51 +0200

    in progress

--- src/utils/handler_tools/context_data_creator.py
+++ src/utils/handler_tools/context_data_creator.py
@@ -1,58 +1,136 @@
-"""This module contains a class structure for creating a context data file. The ContextDataCreator class organizes the
-creation of the context data file."""
-import typing
+"""future data handler. UNFINISHED"""
+import os.path
 import logging
+import typing
+import math
+import itertools
+import numpy as np
+import pandas as pd
+import h5py
 from pathlib import Path
-from functools import partial
+from src.utils.transf_tools.gather import hdf_path_combine
+from setup_logging import setup_logging
+import numpy.typing as npt
+import dateutil.parser
+import time
 from multiprocessing.pool import ThreadPool
 from multiprocessing import Lock
 from multiprocessing.synchronize import Lock as Lock_dtype
-import h5py
-from src.utils.handler_tools.feature import EventDataFeature
+import functools
+import feature_funcs
 
-logger = logging.getLogger(__name__)
+setup_logging()
+log = logging.getLogger("test_handler")
+log.setLevel("DEBUG")
 
+class Feature:
+    def __init__(self, name: str,  func: typing.Callable[[Path, str], typing.Any], output_dtype: type, hdf_path: str = "/"):
+        self.name: str = name
+        self.dataset_name: str = hdf_path  # hdf_grp can also be None
+        self.func: typing.Callable = func
+        self.dtype = output_dtype
 
-def task(feature: EventDataFeature,
-         ed_file_path: Path,
-         dest_file_path: Path,
-         write_lock: Lock_dtype) -> None:
-    """
-    One task is calculating one feature for the whole dataset.
-    :param feature: the Feature object to calculate
-    :param src_file_path: the file path of the source file
-    :param dest_file_path: the file path of the destination file, where the features will be stored
-    :param write_lock: the lock for writing into the destination file path
-    """
-    logger.debug("calculate feature %s for %s", feature.name, feature.dest_hdf_path)
-    vec = feature.apply(ed_file_path)
-    logger.debug("start writing feature %s for %s", feature.name, feature.dest_hdf_path)
+    def initalize(self, file_path: Path, length: int):
+        with h5py.File(file_path, "a") as file:
+            grp = file.require_group(self.dataset_name)
+            grp.create_dataset(name = self.name, shape=(length,), dtype=self.dtype, chunks=True)
+            if not self.func.__doc__ is None:
+                grp[self.name].attrs.create(name="funcion_doc", data=self.func.__doc__)
+            else:
+                raise RuntimeWarning(f"there is no function doc string for function {self.func.__name__} {self.name}. This is added as a feature description.")
+
+    def apply(self, file_path: Path, hdf_path: str):
+
+        path = os.path.join(hdf_path, self.dataset_name)
+        log.debug("%s and %s-> %s",hdf_path, self.dataset_name, path)
+        return self.func(file_path, path)
+
+def task(grp_name: str, index: int, src_file_path: Path, dest_file_path: Path, features: typing.Iterable[Feature], write_lock: Lock_dtype):
+    # CALC
+    calculated_features = {}
+    for feature in features:
+        calculated_features[feature] = feature.apply(src_file_path, grp_name)
+    # WRITE
     with write_lock:
-        feature.write(dest_file_path, vec)
-
+        with h5py.File(dest_file_path, "r+") as dest_file:
+            for feature, value in calculated_features.items():
+                hdf_path = os.path.join(grp_name, feature.dataset_name)
+                log.debug("hdf dest path: %s",hdf_path)
+                dest_file[hdf_path][index] = value
 
 class ContextDataCreator:
-    """operates the creation of the context data file (a file filled with calculated features for each group in the
-     input file."""
-
-    def __init__(self, ed_file_path: Path,
-                 td_file_path: Path,
-                 dest_file_path: Path,
-                 get_features: typing.Callable[[], typing.Iterable[EventDataFeature]]):
-        self.ed_file_path: Path = ed_file_path
-        self.td_file_path: Path = td_file_path
+    def __init__(self, src_file_path: Path, dest_file_path: Path, features: typing.Iterable[Feature]):
+        self.src_file_path: Path = src_file_path
         self.dest_file_path: Path = dest_file_path
-        self.get_event_data_features: typing.Callable[[], typing.Iterable[EventDataFeature]] = get_features
-        with h5py.File(self.ed_file_path, "r") as file:
+        self.features: typing.Iterable[Feature] = features
+        with h5py.File(self.src_file_path, "r") as file:
             self.len: int = len(file)  # = number of keys
 
-    def calc_features(self, num_threads: int = 4):
-        """assigns the tasks (calculating each feature) toa bunch of threads.
-        :param num_threads: the number of threads for multiprocessing"""
+    def init_features(self):
+        h5py.File(self.dest_file_path, "w").close()
+        for feature in self.features:
+            feature.initalize(self.dest_file_path, self.len)
+
+    def calc_features(self, num_iter):
         write_lock = Lock()
-        with ThreadPool(num_threads) as pool:
-            partial_task = partial(task,
-                                   ed_file_path=self.ed_file_path,
-                                   dest_file_path=self.dest_file_path,
-                                   write_lock=write_lock)
-            pool.map(partial_task, self.get_event_data_features())
+        #with h5py.File(self.src_file_path, "r") as file:
+        #    event_keys = list(file.keys())
+        with ThreadPool(1) as pool, h5py.File(self.src_file_path, "r") as file:
+            partial_task = functools.partial(task, src_file_path=self.src_file_path,
+                                             dest_file_path=self.dest_file_path,
+                                             features=self.features,
+                                             write_lock=write_lock)
+            pool.starmap(partial_task, zip(file.keys(), range(num_iter)))  # itertools.count(0)
+
+
+def create_context_data(source_file_path: Path, dest_file_path: Path):
+    features: typing.Iterable[Feature] = []
+    features += [Feature("pulse_length", feature_funcs.pulse_length,                 float,                     hdf_path=chn) for chn in ['PEI Amplitude', 'PKI Amplitude', 'PSI Amplitude', 'PSR Amplitude']]
+    features += [Feature(is_type,        feature_funcs.log_type_translator(is_type), bool                                   ) for is_type in ["is_log", "is_bdin40ms", "is_bdin20ms", "is_bd"]]
+    features += [Feature("timestamp",    feature_funcs.get_timestamp,                h5py.opaque_dtype('M8[us]')            )]
+    cd_creator = ContextDataCreator(source_file_path, dest_file_path, features)
+    t0 = time.time()
+    cd_creator.init_features()
+    t1 = time.time()
+    num_iter=10
+    cd_creator.calc_features(num_iter=num_iter)
+    log.info("init took %ssek and clac took %ssek (so %s per iter)", t1 - t0, time.time() - t1, (time.time() - t1) / num_iter)
+
+
+if __name__=='__main__':
+    ed_links_file_path = Path("~/output_files/EventDataExtLinks.hdf").expanduser()
+    destination_file_path = Path("~/output_files/context_data.hdf").expanduser()
+    create_context_data(ed_links_file_path, destination_file_path)
+
+"""
+def create_context_data_old(source_file_path: Path, dest_file_path: Path):
+    with h5py.File(source_file_path, mode="r") as source_file, \
+         h5py.File(destination_file_path, mode="w") as dest_file:
+
+        length = source_file.__len__()
+
+        ds_timestamp = dest_file.create_dataset("Timestamp", shape=(length,), dtype=h5py.opaque_dtype("<M8[us]"), chunks=True)
+
+        label_name_dict = {0:'is_log', 1:'is_in40ms', 2:'is_in20ms', 3:'is_bd'}
+        for name in label_name_dict.values():
+            dest_file.create_dataset(name=name, data=np.zeros(shape=(length,), dtype=bool), chunks=True)
+
+
+        def pulse_length(df: pd.DataFrame):
+            threshold = df.max(axis=0) / 2
+            acquisation_window: float = 2e-6
+            return (df > threshold).sum() / df.shape[0] * acquisation_window
+
+        for index, key in zip(range(10), source_file.keys()):  # zip(itertools.count(0), source_file.keys()):
+            if math.fmod(index, 1000) == 0: print(index)
+            ret = {}
+            grp = source_file[key]
+            ret["Timestamp"] = grp.attrs["Timestamp"][:-1]
+            label = source_file[key].attrs["Log Type"]
+            ret[label_name_dict[label]] = True
+            df500 = pd.DataFrame([grp[key][:] for key in grp.keys() if grp[key].shape==(500,)], index=[key for key in grp.keys() if grp[key].shape==(500,)]).T
+            df3200 = pd.DataFrame([grp[key][:] for key in grp.keys() if grp[key].shape==(3200,)], index=[key for key in grp.keys() if grp[key].shape==(3200,)]).T
+            amplitude_channels = ['PEI Amplitude', 'PKI Amplitude', 'PSI Amplitude', 'PSR Amplitude']
+            ret.update({chn: {"pulse_length": pls_len} for chn, pls_len in zip(amplitude_channels, pulse_length(df3200[amplitude_channels])) })
+            print(ret)
+"""
