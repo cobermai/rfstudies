9ea3058bf467c19bfeddcce0b0dad5dfee22c171
lfischl <lorenz.fischl@cern.ch>
2021-06-11 17:31:36 +0200

    data handler work in progress Some adjustments to fit pipeline where already made

--- src/utils/handler_tools/context_data_creator.py
+++ src/utils/handler_tools/context_data_creator.py
@@ -1,136 +1,108 @@
-"""future data handler. UNFINISHED"""
-import os.path
-import logging
+""""""
 import typing
-import math
-import itertools
-import numpy as np
-import pandas as pd
-import h5py
+import logging
 from pathlib import Path
-from src.utils.transf_tools.gather import hdf_path_combine
-from setup_logging import setup_logging
+from functools import partial
+import numpy as np
 import numpy.typing as npt
-import dateutil.parser
-import time
+import h5py
 from multiprocessing.pool import ThreadPool
 from multiprocessing import Lock
 from multiprocessing.synchronize import Lock as Lock_dtype
-import functools
-import feature_funcs
+import features
+from setup_logging import setup_logging
+from src.utils.hdf_tools import hdf_path_combine
 
 setup_logging()
 log = logging.getLogger("test_handler")
 log.setLevel("DEBUG")
 
+
 class Feature:
-    def __init__(self, name: str,  func: typing.Callable[[Path, str], typing.Any], output_dtype: type, hdf_path: str = "/"):
+    """A feature is a statistical property of a time series (min, max, mean, pulse_amplitude, pulse_length, etc).
+    One object represents one Feature and can be applied on an hdf5 dataset via the apply function"""
+
+    def __init__(self,
+                 name: str,
+                 func: typing.Callable[[Path, str], typing.Any],
+                 output_dtype: type,
+                 hdf_path: str = "/",
+                 info: str = None):
         self.name: str = name
-        self.dataset_name: str = hdf_path  # hdf_grp can also be None
         self.func: typing.Callable = func
         self.dtype = output_dtype
+        self.dataset_name: str = hdf_path  # hdf_grp can also be None
+        if info is None:
+            raise RuntimeWarning("""It is recommended to add an info text to describe the feature function.
+                                    Maybe you can use func.__doc__?""")
+        self.info_text = info
 
-    def initalize(self, file_path: Path, length: int):
-        with h5py.File(file_path, "a") as file:
+    def write(self, dest_file_path: Path, data: npt.ArrayLike) -> None:
+        """creates an hdf-dataset and fill the data into it.
+        If the group does not exist yet, it also creates an hdf-group of the channel name where the feature belongs to.
+        :param dest_file_path: the file path of the destination file
+        :param data: the length the hdf-dataset should have
+        """
+        with h5py.File(dest_file_path, "a") as file:
             grp = file.require_group(self.dataset_name)
-            grp.create_dataset(name = self.name, shape=(length,), dtype=self.dtype, chunks=True)
-            if not self.func.__doc__ is None:
-                grp[self.name].attrs.create(name="funcion_doc", data=self.func.__doc__)
-            else:
-                raise RuntimeWarning(f"there is no function doc string for function {self.func.__name__} {self.name}. This is added as a feature description.")
+            grp.create_dataset(name=self.name, data=data, dtype=self.dtype, chunks=True)
+            grp[self.name].attrs.create(name="info", data=self.info_text)
 
-    def apply(self, file_path: Path, hdf_path: str):
+    def apply(self, src_file_path: Path) -> npt.ArrayLike:
+        """
+        applies the function of the feature to every hdf-object / self.dataset_name. and returns a vector with the
+        calculated features
+        :param src_file_path: file path of the source file (hdf file)
+        :return: numpy array of datatype self.dtype
+        """
+        with h5py.File(src_file_path, "r") as file:
+            ret_vec = np.empty(shape=(file.__len__(),), dtype=self.dtype)
+            for key, index in zip(file.keys(), range(1000)):
+                ret_vec[index] = self.func(src_file_path, hdf_path_combine(key, self.dataset_name))
+        return ret_vec
 
-        path = os.path.join(hdf_path, self.dataset_name)
-        log.debug("%s and %s-> %s",hdf_path, self.dataset_name, path)
-        return self.func(file_path, path)
 
-def task(grp_name: str, index: int, src_file_path: Path, dest_file_path: Path, features: typing.Iterable[Feature], write_lock: Lock_dtype):
-    # CALC
-    calculated_features = {}
-    for feature in features:
-        calculated_features[feature] = feature.apply(src_file_path, grp_name)
-    # WRITE
+def task(feature: Feature, src_file_path: Path, dest_file_path: Path, write_lock: Lock_dtype) -> None:
+    """
+    One task is calculating one feature for the whole dataset.
+    :param feature: the Feature object to calculate
+    :param src_file_path: the file path of the source file
+    :param dest_file_path: the file path of the destination file, where the features will be stored
+    :param write_lock: the lock for writing into the destination file path
+    """
+    vec = feature.apply(src_file_path)
+    log.info(f"finished calculations for {feature.name}")
     with write_lock:
-        with h5py.File(dest_file_path, "r+") as dest_file:
-            for feature, value in calculated_features.items():
-                hdf_path = os.path.join(grp_name, feature.dataset_name)
-                log.debug("hdf dest path: %s",hdf_path)
-                dest_file[hdf_path][index] = value
+        feature.write(dest_file_path, vec)
+
 
 class ContextDataCreator:
-    def __init__(self, src_file_path: Path, dest_file_path: Path, features: typing.Iterable[Feature]):
+    """operates the creation of the context data file (a file filled with calculated features for each group in the
+     input file."""
+    def __init__(self, src_file_path: Path,
+                 dest_file_path: Path,
+                 get_features: typing.Callable[[], typing.Iterable[Feature]]):
         self.src_file_path: Path = src_file_path
         self.dest_file_path: Path = dest_file_path
-        self.features: typing.Iterable[Feature] = features
+        self.get_features: typing.Callable[[], typing.Iterable[Feature]] = get_features
         with h5py.File(self.src_file_path, "r") as file:
             self.len: int = len(file)  # = number of keys
 
-    def init_features(self):
-        h5py.File(self.dest_file_path, "w").close()
-        for feature in self.features:
-            feature.initalize(self.dest_file_path, self.len)
-
-    def calc_features(self, num_iter):
+    def calc_features(self, num_threads: int = 4):
+        """assigns the tasks (calculating each feature) toa bunch of threads.
+        :param num_threads: the number of threads for multiprocessing"""
         write_lock = Lock()
-        #with h5py.File(self.src_file_path, "r") as file:
-        #    event_keys = list(file.keys())
-        with ThreadPool(1) as pool, h5py.File(self.src_file_path, "r") as file:
-            partial_task = functools.partial(task, src_file_path=self.src_file_path,
-                                             dest_file_path=self.dest_file_path,
-                                             features=self.features,
-                                             write_lock=write_lock)
-            pool.starmap(partial_task, zip(file.keys(), range(num_iter)))  # itertools.count(0)
+        with ThreadPool(num_threads) as pool:
+            partial_task = partial(task, src_file_path=self.src_file_path,
+                                   dest_file_path=self.dest_file_path,
+                                   write_lock=write_lock)
+            pool.map(partial_task, self.get_features())
 
 
-def create_context_data(source_file_path: Path, dest_file_path: Path):
-    features: typing.Iterable[Feature] = []
-    features += [Feature("pulse_length", feature_funcs.pulse_length,                 float,                     hdf_path=chn) for chn in ['PEI Amplitude', 'PKI Amplitude', 'PSI Amplitude', 'PSR Amplitude']]
-    features += [Feature(is_type,        feature_funcs.log_type_translator(is_type), bool                                   ) for is_type in ["is_log", "is_bdin40ms", "is_bdin20ms", "is_bd"]]
-    features += [Feature("timestamp",    feature_funcs.get_timestamp,                h5py.opaque_dtype('M8[us]')            )]
-    cd_creator = ContextDataCreator(source_file_path, dest_file_path, features)
-    t0 = time.time()
-    cd_creator.init_features()
-    t1 = time.time()
-    num_iter=10
-    cd_creator.calc_features(num_iter=num_iter)
-    log.info("init took %ssek and clac took %ssek (so %s per iter)", t1 - t0, time.time() - t1, (time.time() - t1) / num_iter)
-
-
-if __name__=='__main__':
-    ed_links_file_path = Path("~/output_files/EventDataExtLinks.hdf").expanduser()
+if __name__ == '__main__':
     destination_file_path = Path("~/output_files/context_data.hdf").expanduser()
-    create_context_data(ed_links_file_path, destination_file_path)
-
-"""
-def create_context_data_old(source_file_path: Path, dest_file_path: Path):
-    with h5py.File(source_file_path, mode="r") as source_file, \
-         h5py.File(destination_file_path, mode="w") as dest_file:
-
-        length = source_file.__len__()
-
-        ds_timestamp = dest_file.create_dataset("Timestamp", shape=(length,), dtype=h5py.opaque_dtype("<M8[us]"), chunks=True)
-
-        label_name_dict = {0:'is_log', 1:'is_in40ms', 2:'is_in20ms', 3:'is_bd'}
-        for name in label_name_dict.values():
-            dest_file.create_dataset(name=name, data=np.zeros(shape=(length,), dtype=bool), chunks=True)
-
-
-        def pulse_length(df: pd.DataFrame):
-            threshold = df.max(axis=0) / 2
-            acquisation_window: float = 2e-6
-            return (df > threshold).sum() / df.shape[0] * acquisation_window
-
-        for index, key in zip(range(10), source_file.keys()):  # zip(itertools.count(0), source_file.keys()):
-            if math.fmod(index, 1000) == 0: print(index)
-            ret = {}
-            grp = source_file[key]
-            ret["Timestamp"] = grp.attrs["Timestamp"][:-1]
-            label = source_file[key].attrs["Log Type"]
-            ret[label_name_dict[label]] = True
-            df500 = pd.DataFrame([grp[key][:] for key in grp.keys() if grp[key].shape==(500,)], index=[key for key in grp.keys() if grp[key].shape==(500,)]).T
-            df3200 = pd.DataFrame([grp[key][:] for key in grp.keys() if grp[key].shape==(3200,)], index=[key for key in grp.keys() if grp[key].shape==(3200,)]).T
-            amplitude_channels = ['PEI Amplitude', 'PKI Amplitude', 'PSI Amplitude', 'PSR Amplitude']
-            ret.update({chn: {"pulse_length": pls_len} for chn, pls_len in zip(amplitude_channels, pulse_length(df3200[amplitude_channels])) })
-            print(ret)
-"""
+    h5py.File(destination_file_path, "w").close()
+    cd_creator = ContextDataCreator(src_file_path=Path("~/output_files/EventDataExtLinks.hdf").expanduser(),
+                                    dest_file_path=destination_file_path,
+                                    get_features=features.get_features)
+    cd_creator.calc_features()
--- src/utils/handler_tools/union.py
+++ src/utils/handler_tools/union.py
@@ -1,26 +1,19 @@
-import os.path
-from pathlib import Path
-from typing import Union
+import typing
 import logging
-from datetime import datetime
-import math
-import itertools
-import numpy as np
-import pandas as pd
-import h5py
+from functools import partial
 from pathlib import Path
-from src.utils.transf_tools.gather import hdf_path_combine
+from dateutil.parser import parse
+import numpy as np
+import h5py
 from setup_logging import setup_logging
+from src.utils.hdf_tools import get_datasets
+
 setup_logging()
 LOG = logging.getLogger("test_handler")
-import numpy.typing as npt
-from dateutil.parser import parse
-from functools import partial
-import time
 
 
-
-def is_datetime(val):
+def is_datetime(val: typing.Any):
+    """returns True if val can be parsed by dateutil.parser.parse() and False if not."""
     try:
         parse(val)
     except (TypeError, ValueError):
@@ -29,46 +22,54 @@ def is_datetime(val):
         ret = True
     return ret
 
-def get_datetime_converter_for(val):
+
+def get_datetime_converter_for(val: typing.Union[str, bytes]) -> typing.Callable:
+    """Creates the fastest date-time converter known for other date-times given in the the same format.
+    :param val: an example value of datetime format.
+    """
     convert = parse
-    if isinstance(val, bytes):
-        def cut_end(ts: bytes, end: bytes) -> bytes:
-            return ts.replace(end, b"")
-        for useless_timezone_ending in [b"Z", b"+00:00"]:
-            if val.endswith(useless_timezone_ending):
-                convert = partial(cut_end, end=useless_timezone_ending)
+
+    def cut_end(ts: bytes, end: bytes) -> bytes:
+        return ts.replace(end, b"")
+
+    for useless_timezone_ending in [b"Z", b"+00:00"]:
+        if isinstance(val, bytes) and val.endswith(useless_timezone_ending):
+            convert = partial(cut_end, end=useless_timezone_ending)
     return convert
 
 
-def union(source_file_path: Path, dest_file_path: Path):
+def union(source_file_path: Path, dest_file_path: Path) -> None:
     """
-    for each dataset_name it unites (concatenates) all hdf-dataset at given depth to one large hdf-dataset in the
-    destination file
-    :param source_file_path:
-    :param destination_file_path:
-    :param depth:
-    :return:
+    In the first layer of the hdf-directory structure there have to be only groups. In each group is required to have
+    the same hdf-datasets, they are refered to as dataset-type.
+    for each dataset-type: unites (concatenates) all datasets of the same dataset-type from all groups.
+    :param source_file_path: file of the un-united groups with same data set names
+    :param dest_file_path: file where the united datasets will be stored
     """
     with h5py.File(source_file_path, mode="r") as source_file, \
-         h5py.File(dest_file_path, mode="a") as dest_file:
+            h5py.File(dest_file_path, mode="a") as dest_file:
         first_grp = source_file.values().__iter__().__next__()
-        for chn in first_grp.keys():
+        for chn in first_grp.keys():  # the channel names are always the same
             example_val = first_grp[chn][0]
             if is_datetime(val=example_val):
                 convert = get_datetime_converter_for(example_val)
-                data = [convert(ts) for path in source_file.keys() for ts in source_file[path][chn][:]] # .astype(str)
+                data = [convert(ts) for path in source_file.keys() for ts in source_file[path][chn][:]]  # .astype(str)
                 data = np.array(data, dtype=np.datetime64)
                 dest_file.create_dataset(name=chn, data=data.astype(dtype=h5py.opaque_dtype(data.dtype)), chunks=True)
             else:
                 data = np.concatenate([source_file[path][chn][:] for path in source_file.keys()])
                 dest_file.create_dataset(name=chn, data=data, chunks=True)
 
-def clean(file_path: Path):
+
+def clean(file_path: Path) -> None:
+    """remove "rows" where there are None values or infinity values in at least one of the "columns", inplace.
+    A column is referred to as a dataset of the hdf file.
+    A row is referred to as the values from all columns with the same index.
+    :param file_path: the path of the hdf  file with the datasets. (already united with unite())"""
     with h5py.File(file_path, "r+") as file:
-        chn_list = [chn for chn in file.keys() if np.issubdtype(file[chn].dtype, np.number)]
-        shape = file.values().__iter__().__next__().shape
+        shape = file.values().__iter__().__next__().shape  # shape of the first dataset
         is_corrupt = np.zeros(shape, dtype=bool)
-        for chn in chn_list:
+        for chn in (chn for chn in file.keys() if np.issubdtype(file[chn].dtype, np.number)):
             is_corrupt |= np.isnan(file[chn][:]) | np.isinf(file[chn][:])
         new_shape = (sum(~is_corrupt),)
         for key in file.keys():
@@ -76,8 +77,14 @@ def clean(file_path: Path):
             file[key].resize(size=new_shape)
             file[key][...] = data
 
-def sort_by(file_path: Path, sort_by_name: str):
+
+def sort_by(file_path: Path, sort_by_name: str) -> None:
+    """
+    sorts all datasets with respect to one specific dataset (sort_by_name), inplace.
+    :param file_path: the path of the hdf  file with the datasets. (already united with unite())
+    :param sort_by_name: name of the dataset to be sorted
+    """
     with h5py.File(file_path, "r+") as file:
         indices_order = file[sort_by_name][:].argsort()
-        for key in file.keys():
+        for key in get_datasets(file_path):
             file[key][...] = file[key][indices_order]
