eda5cf61228218f2f817fb64ac3ae3ea6e583a61
lfischl <lorenz.fischl@cern.ch>
2021-06-16 09:50:34 +0200

    renaming memo -> memory due to dual meaning of memo

--- src/utils/system/dev_tools.py
+++ src/utils/system/dev_tools.py
@@ -4,17 +4,17 @@ import logging
 import psutil
 
 
-def memo_usage(log: logging.Logger):
+def memory_usage(log: logging.Logger):
     """log memory usage of the current process
     :param log: logger for logging the memory usage
     """
     pid = os.getpid()
-    memory_usage = psutil.Process(pid).memory_info()[0] / 2. ** 30
-    log.debug("you are currently using " + memory_usage + "GByte of Memory")
+    mem_usage = psutil.Process(pid).memory_info()[0] / 2. ** 30
+    log.debug("you are currently using " + mem_usage + "GByte of Memory")
 
 
-def memo_usage_print():
+def memory_usage_print():
     """print memory usage of the current process"""
     pid = os.getpid()
-    memory_usage = psutil.Process(pid).memory_info()[0] / 2. ** 30
-    print("you are currently using " + memory_usage + "GByte of Memory")
+    mem_usage = psutil.Process(pid).memory_info()[0] / 2. ** 30
+    print("you are currently using " + mem_usage + "GByte of Memory")
