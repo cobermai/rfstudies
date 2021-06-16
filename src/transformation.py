e05fc22e3d75604b4166f9b741a660524af05bf6
lfischl <lorenz.fischl@cern.ch>
2021-06-16 14:37:31 +0200

    introduced grp var instead of calling file[hdf_paht] multiple times

--- src/transformation.py
+++ src/transformation.py
@@ -22,7 +22,7 @@ def transform(tdms_dir: Path, hdf_dir: Path) -> None:
     """
     Path(hdf_dir, "data").mkdir(parents=False, exist_ok=True)
 
-    # read tdms files and convert them to hdf5 and writing them into hdf_dir/data/
+    # read tdms files, convert them to hdf5 and write them into hdf_dir/data/
     Convert(check_already_converted=True, num_processes=4)\
         .from_tdms(tdms_dir)\
         .to_hdf(hdf_dir / "data").run()
@@ -30,10 +30,11 @@ def transform(tdms_dir: Path, hdf_dir: Path) -> None:
     # Combining all Events and TrendData sets into one hdf5 file with external links if they are not faulty
     def td_func_to_fulfill(file_path: Path, hdf_path: str) -> bool:
         with h5py.File(file_path, "r") as file:
-            ch_shapes: list = [file[hdf_path][key].shape[0] for key in file[hdf_path].keys()]
-            len_equal: bool = all(ch_shape == ch_shapes[0] for ch_shape in ch_shapes)
-            num_of_samples: int = 35
-            return len_equal and len(file[hdf_path].keys()) == num_of_samples
+            grp = file[hdf_path]
+            ch_shapes = [grp[key].shape[0] for key in grp.keys()]
+            len_equal = all(ch_shape == ch_shapes[0] for ch_shape in ch_shapes)
+            num_of_samples = 35
+            return len_equal and grp.len == num_of_samples
 
     gather(src_file_paths=hdf_dir.glob("data/Trend*.hdf"),
            dest_file_path=hdf_dir / "TrendDataExtLinks.hdf",
@@ -43,27 +44,28 @@ def transform(tdms_dir: Path, hdf_dir: Path) -> None:
 
     def ed_func_to_fulfill(file_path: Path, hdf_path: str) -> bool:
         with h5py.File(file_path, "r") as file:
-            ch_len: list = [file[hdf_path][key].shape[0] for key in file[hdf_path].keys()]
+            grp = file[hdf_path]
+            ch_len = [ch.shape[0] for ch in grp.items()]
 
-            acquisition_window: float = 2e-6  # time period of one event is 2 microseconds
+            acquisition_window = 2e-6  # time period of one event is 2 microseconds
 
             # acquisition card NI-5772 see https://www.ni.com/en-us/support/model.ni-5772.html
-            sampling_frequency_ni5772: int = int(1.6e8)  # sampling frequency of the acquisition card
-            num_of_values_ni5772: int = int(acquisition_window * sampling_frequency_ni5772)
-            number_of_signals_monitored_with_ni5772: int = 8
+            sampling_frequency_ni5772 = 1.6e8  # sampling frequency of the acquisition card
+            num_of_values_ni5772 = acquisition_window * sampling_frequency_ni5772
+            number_of_signals_monitored_with_ni5772 = 8
 
             # acquisition card NI-5761 see https://www.ni.com/en-us/support/model.ni-5761.html
-            sampling_frequency_ni5761: int = int(1.6e9)  # sampling frequency of the acquisition card
-            num_of_values_ni5761: int = int(acquisition_window * sampling_frequency_ni5761)
-            number_of_signals_monitored_with_ni5761: int = 8
+            sampling_frequency_ni5761 = 1.6e9  # sampling frequency of the acquisition card
+            num_of_values_ni5761 = acquisition_window * sampling_frequency_ni5761
+            number_of_signals_monitored_with_ni5761 = 8
 
             def has_smelly_values(data) -> bool:
                 return any(np.isnan(data) | np.isinf(data))
 
-            return file[hdf_path].attrs.get("Timestamp", None) is not None \
+            return grp.attrs.get("Timestamp", None) is not None \
                 and ch_len.count(num_of_values_ni5772) == number_of_signals_monitored_with_ni5772 \
                 and ch_len.count(num_of_values_ni5761) == number_of_signals_monitored_with_ni5761 \
-                and not any(has_smelly_values(file[hdf_path][key][:]) for key in file[hdf_path].keys())
+                and not any(has_smelly_values(ch[:]) for ch in grp.items())
 
     gather(src_file_paths=hdf_dir.glob("data/Event*.hdf"),
            dest_file_path=hdf_dir / "EventDataExtLinks.hdf",
