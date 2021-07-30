8bae7b4593d92ac76a7eeec4853f09368c6b9bfd
hbovbjer <holger.severin.bovbjerg@cern.ch>
2021-07-30 15:00:35 +0200

    added test for Classifier.eval_classification()

--- tests/unit/model/classifiers/test_classifier.py
+++ tests/unit/model/classifiers/test_classifier.py
@@ -1,11 +1,13 @@
-from pathlib import Path
 from collections import namedtuple
 import numpy as np
-import pytest
+import pandas as pd
 from src.model import classifier
 
 
-def test__one_hot():
+def test__one_hot(tmp_path):
+    '''
+    Test one_hot function of class Classifier
+    '''
     # ARRANGE
     data = namedtuple("data", ["X", "y", "idx"])
     X_train = np.empty(1)
@@ -21,7 +23,7 @@ def test__one_hot():
     clf = classifier.Classifier(train_data=train,
                      valid_data=valid,
                      classifier_name="fcn",
-                     output_directory=Path("~/PycharmProjects/mlframework/tests/output"))
+                     output_directory=tmp_path)
 
     y_train_hot_expected = np.array([[0, 1], [1, 0], [0, 1]])
     y_valid_hot_expected = np.array([[1, 0], [0, 1], [1, 0]])
@@ -34,27 +36,66 @@ def test__one_hot():
     assert (y_valid_hot == y_valid_hot_expected).all()
 
 
-    # def test__eval_classifications():
-    #     # ARRANGE
-    #
-    #     X = np.array([[[0, 0, 0], [1, 1, 1]]])
-    #     X_expected = np.array([[[-1, -1, -1], [1, 1, 1]]])
-    #
-    #     # ACT
-    #     X_output = main.scale_data(X)
-    #
-    #     # ASSERT
-    #     assert (X_output == X_expected).all()
-    #
-    #
-    # def test__calc_class_imbalance(y):
-    #     # ARRANGE
-    #
-    #     X = np.array([[[0, 0, 0], [1, 1, 1]]])
-    #     X_expected = np.array([[[-1, -1, -1], [1, 1, 1]]])
-    #
-    #     # ACT
-    #     X_output = main.scale_data(X)
-    #
-    #     # ASSERT
-    #     assert (X_output == X_expected).all()
+def test__eval_classifications(tmp_path):
+    '''
+    Test eval_classifications function of class Classifier
+    '''
+    # ARRANGE
+    data = namedtuple("data", ["X", "y", "idx"])
+    X_train = np.empty(1)
+    y_train = np.array([True, False, True])
+    idx_train = np.empty(1)
+    X_valid = np.empty(1)
+    y_valid = np.array([False, True, False])
+    idx_valid = np.empty(1)
+    X_test = np.empty(1)
+    y_test = np.array([False, False, True])
+    idx_test = np.empty(1)
+
+    train = data(X_train, y_train, idx_train)
+    valid = data(X_valid, y_valid, idx_valid)
+    test = data(X_test, y_test, idx_test)
+
+    clf = classifier.Classifier(train_data=train,
+                                valid_data=valid,
+                                classifier_name="fcn",
+                                output_directory=tmp_path)
+
+    probabilities = np.array([[0.1, 0.7], [0.5, 0.2], [0.3, 0.9]])
+
+    train_time = 2
+
+    balanced_accuracy_expected = 0.75
+    tn_expected, fp_expected, fn_expected, tp_expected = 1, 1, 0, 1
+    bd_rate_expected = tn_expected / (tn_expected + fp_expected)
+    roc_auc_score_expected = 1
+    f1_score_expected = 2/3
+    n_train_healthy_expected = train.y.sum()
+    n_train_bd_expected = len(train.y) - train.y.sum()
+    n_test_healthy_expected = test.y.sum()
+    n_test_bd_expected = len(test.y) - test.y.sum()
+    class_imbalance_train_expected = train.y.sum() / (len(train.y) - sum(train.y))
+    class_imbalance_test_expected = test.y.sum() / (len(test.y) - sum(test.y))
+
+    # ACT
+    clf.train_time = train_time
+    clf.eval_classifications(y_test, probabilities)
+    data_path = tmp_path / "df_metrics.csv"
+    df_results = pd.read_csv(data_path)
+
+    # ASSERT
+    assert (df_results["balanced_accuracy"] == balanced_accuracy_expected).all()
+    assert (df_results["bd_rate"] == bd_rate_expected).all()
+    assert (df_results["roc_auc_score"] == roc_auc_score_expected).all()
+    assert (df_results["f1_score"] == f1_score_expected).all()
+    assert (df_results["train_time"] == train_time).all()
+    assert (df_results["cm_tp"] == tp_expected).all()
+    assert (df_results["cm_tn"] == tn_expected).all()
+    assert (df_results["cm_fp"] == fp_expected).all()
+    assert (df_results["cm_fn"] == fn_expected).all()
+    assert (df_results["n_train_healthy"] == n_train_healthy_expected).all()
+    assert (df_results["n_train_bd"] == n_train_bd_expected).all()
+    assert (df_results["n_test_healthy"] == n_test_healthy_expected).all()
+    assert (df_results["n_test_bd"] == n_test_bd_expected).all()
+    assert (df_results["class_imbalance_train"] == class_imbalance_train_expected).all()
+    assert (df_results["class_imbalance_test"] == class_imbalance_test_expected).all()
