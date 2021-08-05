7af40fa83939aec111fb41d3254eb71b810e9a90
hbovbjer <holger.severin.bovbjerg@cern.ch>
2021-08-05 10:58:09 +0200

    Fix bugs in resnet model

--- src/model/classifiers/resnet2.py
+++ src/model/classifiers/resnet2.py
@@ -8,14 +8,17 @@ class ResnetSubBlock(layers.Layer):
 
     def __init__(self, n_feature_maps):
         super(ResnetSubBlock, self).__init__()
+        # CNN Block 1
         self.cnn1 = CNNBlock(filters=n_feature_maps, kernel_size=8)
+        # CNN Block 2
         self.cnn2 = CNNBlock(filters=n_feature_maps, kernel_size=5)
-        self.conv = layers.Conv1D(filters=n_feature_maps, kernel_size=3 padding='same')
+        # CNN Block 3
+        self.conv = layers.Conv1D(filters=n_feature_maps, kernel_size=3, padding='same')
         self.bn = layers.BatchNormalization()
-        self.shortcut = ShortcutBlock()
+        # Output Block
+        self.shortcut = ShortcutBlock(filters=n_feature_maps, kernel_size=1)
         self.add = layers.Add()
         self.relu = layers.Activation(activation='relu')
-        self.gap = layers.GlobalAveragePooling1D()
 
     def call(self, input_tensor, training=None, mask=None):
         """
@@ -35,7 +38,7 @@ class ResnetSubBlock(layers.Layer):
 class ResnetBlock(layers.Layer):
     """Resnet neural network, initially proposed by https://github.com/hfawaz/dl-4-tsc"""
 
-    def __init__(self, nb_classes):
+    def __init__(self, num_classes):
         super(ResnetBlock, self).__init__()
         self.resnet1 = ResnetSubBlock(n_feature_maps=64)
         self.resnet2 = ResnetSubBlock(n_feature_maps=128)
@@ -43,29 +46,30 @@ class ResnetBlock(layers.Layer):
         self.cnn2 = CNNBlock(filters=128, kernel_size=5)
         self.conv = layers.Conv1D(filters=128, kernel_size=3, padding='same')
         self.bn = layers.BatchNormalization()
-        self.shortcut = ShortcutBlock()
+        self.shortcut = ShortcutBlock(filters=128, kernel_size=1)
         self.add = layers.Add()
+        self.relu = layers.Activation(activation='relu')
         self.gap = layers.GlobalAveragePooling1D()
-        self.out = layers.Dense(nb_classes, activation='softmax')
+        self.out = layers.Dense(num_classes, activation='softmax')
 
     def call(self, input_tensor, training=None, mask=None):
         """
-        Function builds Resnet model from 2 ResnetSubBlocks and a custom resnet subblock where a conv is skipped.
+        Function builds Resnet model from 2 ResnetSubBlocks and a custom resnet sub block where a conv layer is skipped.
         In the end there is a global average pooling layer which feeds the output into a softmax classification layer.
         """
-        # Block 1
+        # Resnet Block 1
         out1 = self.resnet1(input_tensor)
-        # Block 2
+        # Resnet Block 2
         out2 = self.resnet2(out1)
-        # Block 3
+        # Resnet Block 3
         x = self.cnn1(out2)
         y = self.cnn2(x)
         z = self.conv(y)
         z = self.bn(z)
-        shortcut_y = self.bn(x)
+        shortcut_y = self.bn(out2)
         out3 = self.add([shortcut_y, z])
         out3 = self.relu(out3)
-        # output stage
+        # Resnet output Block
         gap = self.gap(out3)
         out = self.out(gap)
         return out
