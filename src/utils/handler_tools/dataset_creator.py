bb8560833dbc20101b9e05ffcd33f72506fc0f4a
hbovbjer <holger.severin.bovbjerg@cern.ch>
2021-08-12 11:29:59 +0200

    Move scale_data and train_valid_test to dataset_creator

--- src/utils/handler_tools/dataset_creator.py
+++ src/utils/handler_tools/dataset_creator.py
@@ -1,7 +1,12 @@
 import typing
 from pathlib import Path
+from collections import namedtuple
+import numpy as np
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from src.xbox2_specific.datasets import simple_select
+from src.xbox2_specific.datasets import XBOX_event_bd20ms
 
 
 def one_hot_encode(y):
@@ -14,6 +19,43 @@ def one_hot_encode(y):
     return enc.fit_transform(y.reshape(-1, 1)).toarray()
 
 
+def scale_data(X):
+    """
+    function scales data for prediction with standard scaler
+    :param X: data array of shape (event, sample, feature)
+    :return: X_scaled: scaled data array of shape (event, sample, feature)
+    """
+    X_scaled = np.zeros_like(X)
+    for feature_index in range(len(X[0, 0, :])):  # Iterate through feature
+        X_scaled[:, :, feature_index] = StandardScaler().fit_transform(X[:, :, feature_index].T).T
+    return X_scaled
+
+
+def train_valid_test_split(X, y, splits: tuple) -> typing.Tuple:
+    """
+    Splits data into training, testing and validation set using random sampling
+    :param X: input data array of shape (event, sample, feature)
+    :param y: output data array of shape (event)
+    :param splits: tuple specifying splitting fractions (training, validation, test)
+    :return: train, valid, test: Tuple with data of type named tuple
+    """
+    if splits[0] == 1:
+        raise ValueError('Training set fraction cannot be 1')
+
+    idx = np.arange(len(X))
+    X_train, X_tmp, y_train, y_tmp, idx_train, idx_tmp = \
+        train_test_split(X, y, idx, train_size=splits[0])
+    X_valid, X_test, y_valid, y_test, idx_valid, idx_test = \
+        train_test_split(X_tmp, y_tmp, idx_tmp, train_size=splits[1] / (1 - (splits[0])))
+
+    data = namedtuple("data", ["X", "y", "idx"])
+    train = data(X_train, y_train, idx_train)
+    valid = data(X_valid, y_valid, idx_valid)
+    test = data(X_test, y_test, idx_test)
+
+    return train, valid, test
+
+
 def load_dataset(data_path: Path, dataset_name: str) -> typing.Tuple:
     """
     Loads the specified data set, does one hot encoding on labels and splits data into train, valid and test set
@@ -23,9 +65,14 @@ def load_dataset(data_path: Path, dataset_name: str) -> typing.Tuple:
     """
     if dataset_name == "simple_select":
         X, y = simple_select.select_data(context_data_file_path=data_path / "context.hdf")
-        X_scaled = simple_select.scale_data(X)
+        X_scaled = scale_data(X)
         y_hot = one_hot_encode(y)
-        train, valid, test = simple_select.train_valid_test_split(X=X_scaled, y=y_hot, splits=(0.7, 0.2, 0.1))
+        train, valid, test = train_valid_test_split(X=X_scaled, y=y_hot, splits=(0.7, 0.2, 0.1))
+    if dataset_name == "XBOX_event_bd20ms":
+        X, y = XBOX_event_bd20ms.select_data(context_data_file_path=data_path / "context.hdf")
+        X_scaled = scale_data(X)
+        y_hot = one_hot_encode(y)
+        train, valid, test = train_valid_test_split(X=X_scaled, y=y_hot, splits=(0.7, 0.2, 0.1))
     else:
         raise AssertionError
 
--- src/xbox2_specific/datasets/simple_select.py
+++ src/xbox2_specific/datasets/simple_select.py
@@ -21,7 +21,6 @@ def select_data(context_data_file_path: Path) -> typing.Tuple[np.ndarray, np.nda
         is_bd_in_next_pulse = file["is_bd_in_20ms"][:]
         is_bd = file["is_bd"][:]
 
-        #
         event_ts = file["Timestamp"][:]
         trend_ts = file["PrevTrendData/Timestamp"][:]
         time_diff = event_ts - trend_ts
@@ -48,39 +47,4 @@ def select_data(context_data_file_path: Path) -> typing.Tuple[np.ndarray, np.nda
     return X, y
 
 
-def scale_data(X):
-    """
-    function scales data for prediction with standard scaler
-    :param X: data array of shape (event, sample, feature)
-    :return: X_scaled: scaled data array of shape (event, sample, feature)
-    """
 
-    X_scaled = np.zeros_like(X)
-    for feature_index in range(len(X[0, 0, :])):  # Iterate through feature
-        X_scaled[:, :, feature_index] = StandardScaler().fit_transform(X[:, :, feature_index].T).T
-    return X_scaled
-
-
-def train_valid_test_split(X, y, splits: tuple) -> typing.Tuple:
-    """
-    Splits data into training, testing and validation set using random sampling
-    :param X: input data array of shape (event, sample, feature)
-    :param y: output data array of shape (event)
-    :param splits: tuple specifying splitting fractions (training, validation, test)
-    :return: train, valid, test: Tuple with data of type named tuple
-    """
-    if splits[0] == 1:
-        raise ValueError('Training set fraction cannot be 1')
-
-    idx = np.arange(len(X))
-    X_train, X_tmp, y_train, y_tmp, idx_train, idx_tmp = \
-        train_test_split(X, y, idx, train_size=splits[0])
-    X_valid, X_test, y_valid, y_test, idx_valid, idx_test = \
-        train_test_split(X_tmp, y_tmp, idx_tmp, train_size=splits[1] / (1 - (splits[0])))
-
-    data = namedtuple("data", ["X", "y", "idx"])
-    train = data(X_train, y_train, idx_train)
-    valid = data(X_valid, y_valid, idx_valid)
-    test = data(X_test, y_test, idx_test)
-
-    return train, valid, test
