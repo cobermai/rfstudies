0669875e03dfcb1dd4ae26cab8ecd58d268d428d
hbovbjer <holger.severin.bovbjerg@cern.ch>
2021-08-12 11:30:34 +0200

    Move scale_data and train_valid_test to dataset_creator

--- tests/unit/utils/handler_tools/test_dataset_creator.py
+++ tests/unit/utils/handler_tools/test_dataset_creator.py
@@ -25,6 +25,53 @@ def test__one_hot(y, y_one_hot_expected):
     assert (y_one_hot == y_one_hot_expected).all()
 
 
+def test__scale_data():
+    """
+    Test scale_data() function
+    """
+    # ARRANGE
+
+    X = np.array([[[0, 0, 0], [1, 1, 1]]])
+    X_expected = np.array([[[-1, -1, -1], [1, 1, 1]]])
+
+    # ACT
+    X_output = dataset_creator.scale_data(X)
+    print(X_output)
+    # ASSERT
+    assert (X_output == X_expected).all()
+
+
+def test__train_valid_test_split():
+    """
+    Test train_valid_test_split() function
+    """
+    # ARRANGE
+    X = np.array(range(10, 20))
+    y = np.array(range(20, 30))
+    splits_expected = 0.7, 0.1, 0.2
+
+    # ACT
+    train, valid, test = dataset_creator.train_valid_test_split(X, y, splits_expected)
+
+    # ASSERT
+    assert set(X) == set(train.X).union(valid.X).union(test.X)
+    assert set(y) == set(train.y).union(valid.y).union(test.y)
+    length = len(X)
+    assert set(range(length)) == set(train.idx).union(valid.idx).union(test.idx)
+    splits_output = len(train.X) / length, len(valid.X) / length, len(test.X) / length
+    assert splits_output == splits_expected
+
+
+def test__train_valid_test_split_errors():
+    # ARRANGE
+    X = np.array(range(10, 20))
+    y = np.array(range(20, 30))
+
+    # ACT
+    with pytest.raises(ValueError):
+        dataset_creator.train_valid_test_split(X, y, (1, 0, 0))
+
+
 @pytest.mark.skip(reason="no way of currently testing this")
 def test__load_dataset(tmp_path):
     d = tmp_path / "context.hdf"
--- tests/unit/xbox2_specific/datasets/test_simple_select.py
+++ tests/unit/xbox2_specific/datasets/test_simple_select.py
@@ -1,65 +1,34 @@
+from pathlib import Path
+import h5py
 import numpy as np
 import pytest
+from src.utils.handler_tools.context_data_writer import ColumnWiseContextDataWriter
 from src.xbox2_specific.datasets import simple_select
 
 
 @pytest.mark.skip(reason="no way of currently testing this")
 def test__select_data(tmp_path):
     # ARRANGE
-    X_expected = np.array([1, 2, 3])
-    y_expected = np.array([4, 5, 6])
+    context = h5py.File('context.hdf', 'w')
+    with context as f:
+        f.create_dataset("is_bd_in_40ms", (1, 0, 1))
+        f.create_dataset("is_bd_in_20ms", (0, 1, 0))
+        f.create_dataset("is_bd", (0, 0, 1))
+        f.create_dataset("Timestamp", (0.01, 0.03, 0.05))
+        f.create_dataset("PrevTrendData/Timestamp", (0.02, 0.04, 0.06))
+        f.create_dataset("clic_label/is_healthy", (1, 0, 0))
 
-    # Create dummy file in tmp path
+    dest_file_path = Path(tmp_path / 'context_file')
+    column_wise_handler = ColumnWiseContextDataWriter(dest_file_path, length=10)
+    for feature in features:
+        column_wise_handler.write_column(feature)
 
     # ACT
     X, y = simple_select.select_data(tmp_path)
+
     # ASSERT
     assert X == X_expected
     assert y == y_expected
 
 
-def test__scale_data():
-    """
-    Test scale_data() function
-    """
-    # ARRANGE
 
-    X = np.array([[[0, 0, 0], [1, 1, 1]]])
-    X_expected = np.array([[[-1, -1, -1], [1, 1, 1]]])
-
-    # ACT
-    X_output = simple_select.scale_data(X)
-    print(X_output)
-    # ASSERT
-    assert (X_output == X_expected).all()
-
-
-def test__train_valid_test_split():
-    """
-    Test train_valid_test_split() function
-    """
-    # ARRANGE
-    X = np.array(range(10, 20))
-    y = np.array(range(20, 30))
-    splits_expected = 0.7, 0.1, 0.2
-
-    # ACT
-    train, valid, test = simple_select.train_valid_test_split(X, y, splits_expected)
-
-    # ASSERT
-    assert set(X) == set(train.X).union(valid.X).union(test.X)
-    assert set(y) == set(train.y).union(valid.y).union(test.y)
-    length = len(X)
-    assert set(range(length)) == set(train.idx).union(valid.idx).union(test.idx)
-    splits_output = len(train.X) / length, len(valid.X) / length, len(test.X) / length
-    assert splits_output == splits_expected
-
-
-def test__train_valid_test_split_errors():
-    # ARRANGE
-    X = np.array(range(10, 20))
-    y = np.array(range(20, 30))
-
-    # ACT
-    with pytest.raises(ValueError):
-        simple_select.train_valid_test_split(X, y, (1, 0, 0))
