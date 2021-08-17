daa26fd4fc2356ea58c4deb86a39752fd1ca0545
Holger Severin Bovbjerg <31927268+HolgerBovbjerg@users.noreply.github.com>
2021-08-17 15:13:16 +0200

    Add test for custom layers

--- tests/unit/model/classifiers/layers/test_cnn_dropout.py
+++ tests/unit/model/classifiers/layers/test_cnn_dropout.py
@@ -1,16 +1,16 @@
 import numpy as np
 import tensorflow as tf
-from src.model.classifiers.layers import cnn
+from src.model.classifiers.layers import cnn_dropout
 
 
-def test__cnn():
+def test__cnn_dropout():
     """
     Function that tests the structure CNNLayer
     """
     # ARRANGE
-    layer_cnn = cnn.CNNBlock(2, 2)
+    layer_cnn_dropout = cnn_dropout.CNNDropoutBlock(2, 2)
     x = tf.ones((3, 3, 3))
     # ACT
-    y = layer_cnn(x)
+    y = layer_cnn_dropout(x)
     # ASSERT
     assert not np.isnan(y).all()
--- tests/unit/model/classifiers/layers/test_shortcut.py
+++ tests/unit/model/classifiers/layers/test_shortcut.py
@@ -1,16 +1,16 @@
 import numpy as np
 import tensorflow as tf
-from src.model.classifiers.layers import cnn
+from src.model.classifiers.layers import shortcut
 
 
-def test__cnn():
+def test__shortcut():
     """
     Function that tests the structure CNNLayer
     """
     # ARRANGE
-    layer_cnn = cnn.CNNBlock(2, 2)
+    layer_shortcut = shortcut.ShortcutBlock(2, 2)
     x = tf.ones((3, 3, 3))
     # ACT
-    y = layer_cnn(x)
+    y = layer_shortcut(x)
     # ASSERT
     assert not np.isnan(y).all()
