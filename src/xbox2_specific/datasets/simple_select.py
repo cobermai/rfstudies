378db941cc6cbe832b768688e395bba9aacf9821
hbovbjer <holger.severin.bovbjerg@cern.ch>
2021-08-17 11:31:47 +0200

    Refactor select_data by moving parts into separate functions.

--- src/xbox2_specific/datasets/simple_select.py
+++ src/xbox2_specific/datasets/simple_select.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 from src.utils.hdf_tools import hdf_to_df_selection
 
+
 def one_hot_encode(y):
     """
     Transforms the labels from integers to one hot vectors
@@ -30,52 +31,94 @@ def scale_data(X):
     return X_scaled
 
 
+def read_hdf_dataset(file: h5py.File, key: str):
+    """
+    Read dataset from hdf file
+    :param file: h5py File object with read access
+    :param key: string specifying key for dataset to read
+    :return: array containing contents of h5py dataset
+    """
+    dataset = file[key]
+    if not isinstance(dataset, h5py.Dataset):
+        raise ValueError("Specified key does not yield a hdf dataset")
+    return dataset[:]
 
 
-def read_data(file, key):
-    return file[key][:]
-
-
-def create_filter(event_timestamps, trend_timestamps, time_threshold):
+def create_filter(event_timestamps: np.datetime64, trend_timestamps: np.datetime64, time_threshold: float) -> bool:
+    """
+    Creates filter from event data and trend data timestamps for filtering healthy pulses
+    with time diff more than threshold.
+    :param event_timestamps: array of event data timestamps
+    :param trend_timestamps: array of trend data timestamps
+    :param time_threshold: threshold in seconds
+    :return: filter for boolean indexing
+    """
     time_diff = event_timestamps - trend_timestamps
     time_diff_threshold = pd.to_timedelta(time_threshold, "s")
     filter_timestamp_diff = time_diff < time_diff_threshold
     return filter_timestamp_diff
 
 
-def select_data(context_data_file_path: Path) -> typing.Tuple[np.ndarray, np.ndarray]:
+def load_X_data(df: pd.DataFrame, index: pd.Index) -> np.ndarray:
     """
-    returns all breakdown events (no distinction between runs) and 2.5% of the healthy events (chosen randomly).
-    filters out healthy events where the prev trend data is further away than two seconds.
-    :return: X and y prepared for machine learning
+    Loads data to numpy array from dataframe
+    :param df: Pandas dataframe containing data
+    :param index: Pandas index variable with features for use in machine learning
+    :return X: Numpy array with data loaded from dataframe
+    """
+    X = df[index].to_numpy(dtype=float)
+    X = X[..., np.newaxis]
+    X = np.nan_to_num(X)
+    return X
+
+
+def create_breakdown_selection_filter(context_data_file_path: Path,
+                                      selection_list: typing.List[str]) -> typing.List[bool]:
+    """
+    Creates a filter for selecting breakdown events
+    :param context_data_file_path: path to context data file
+    :param selection_list: List of features used for selecting breakdowns
+    :return selection: boolean filter for selecting breakdown events
     """
     with h5py.File(context_data_file_path, "r") as file:
         # load relevant data from context file
-        is_bd_in_two_pulses = read_data(file, "is_bd_in_40ms")
-        is_bd_in_next_pulse = read_data(file, "is_bd_in_20ms")
-        is_bd = read_data(file, "is_bd")
-        event_timestamps = read_data(file, "Timestamp")
-        trend_timestamp = read_data(file, "PrevTrendData/Timestamp")
+        features_read = []
+        for key in selection_list:
+            features_read.append(read_hdf_dataset(file, key))
+        selection = features_read[0]
+        for i in range(1, len(features_read)):
+            selection = selection | features_read[i]
 
-        # filter healthy pulses with a time difference to previous trend data more than 2 s
-        # only define healthy pulses with a time difference to the previous trend data of < 2s
+        event_timestamps = read_hdf_dataset(file, "Timestamp")
+        trend_timestamp = read_hdf_dataset(file, "PrevTrendData/Timestamp")
+
+        # only define healthy pulses with a time difference to the previous trend data of less than 2 s
         filter_timestamp_diff = create_filter(event_timestamps, trend_timestamp, 2)
-        is_healthy = file["clic_label/is_healthy"][:] & filter_timestamp_diff
-
-        # select all breakdown events and the two directly preceding pulses
-        selection = (is_bd_in_two_pulses | is_bd_in_next_pulse | is_bd)
+        is_healthy = read_hdf_dataset(file, "clic_label/is_healthy") & filter_timestamp_diff
 
         # select 2.5% of the healthy pulses randomly
         selection[is_healthy] = np.random.choice(a=[True, False], size=(sum(is_healthy),), p=[0.025, 0.975])
+        return selection
 
+
+def select_data(context_data_file_path: Path) -> typing.Tuple[np.ndarray, np.ndarray]:
+    """
+    Returns all breakdown events (no distinction between runs) and 2.5% of the healthy events (chosen randomly).
+    filters out healthy events where the prev trend data is further away than two seconds.
+    :param context_data_file_path: Path to context.hdf file
+    :return X_scaled, y_hot: X and y prepared for machine learning
+    """
+    # select all breakdown events and the two directly preceding pulses
+    breakdown_selection = ["is_bd_in_40ms", "is_bd_in_20ms", "is_bd"]
+    selection = create_breakdown_selection_filter(context_data_file_path, breakdown_selection)
     df = hdf_to_df_selection(context_data_file_path, selection=selection)
-
-    clm_for_training = df.columns.difference(pd.Index(["Timestamp", "PrevTrendData__Timestamp", "is_bd", "is_healthy",
-                                                       "is_bd_in_20ms", "is_bd_in_40ms"]))
-    X = df[clm_for_training].to_numpy(dtype=float)
-    X = X[..., np.newaxis]
-    X = np.nan_to_num(X)
+    # Use all features apart from
+    feature_names = ["Timestamp", "PrevTrendData__Timestamp", "is_bd", "is_healthy", "is_bd_in_20ms", "is_bd_in_40ms"]
+    clm_for_training = df.columns.difference(pd.Index(feature_names))
+    # Load features and labels
+    X = load_X_data(df, clm_for_training)
     y = df["is_healthy"].to_numpy(dtype=bool)
+
     X_scaled = scale_data(X)
     y_hot = one_hot_encode(y)
     return X_scaled, y_hot
