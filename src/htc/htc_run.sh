db3f51f5e372116cbad890328aabbd70accea1bc
Christoph Obermair <christoph.obermair@cern.ch>
2021-08-19 11:46:24 +0000

    Deleted src/htc/htc_run.sh, src/htc/htc_sensitivity.sh, src/htc/htc_submit.sub files

--- src/htc/htc_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/cobermai/PycharmProjects/mlframework/src/htc
-python -m virtualenv myvenvpip install -r requirements.txtpython xbox2_main.py --file_path=/home/cobermai/PycharmProjects/mlframework --output_path=/home/cobermai/PycharmProjects/mlframework/src/output/2021-08-18T16.34.01$_(ProcId)
