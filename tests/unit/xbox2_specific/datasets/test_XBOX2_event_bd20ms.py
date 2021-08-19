c15cdce23ca64966b7e368c46d75cadbcad5a3eb
Holger Severin Bovbjerg <31927268+HolgerBovbjerg@users.noreply.github.com>
2021-08-19 16:40:32 +0200

    Add tests for XBOX2 datasets

--- tests/unit/xbox2_specific/datasets/test_XBOX2_event_bd20ms.py
+++ tests/unit/xbox2_specific/datasets/test_XBOX2_event_bd20ms.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import pytest
 from src.utils import dataset_creator
-from src.xbox2_specific.datasets import simple_select
+from src.xbox2_specific.datasets import XBOX2_event_bd20ms
 
 
 @pytest.mark.parametrize("y, \
@@ -20,7 +20,7 @@ def test__one_hot(y, y_one_hot_expected):
     Test one_hot function of dataset_creator
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_event_bd20ms.XBOX2EventBD20msSelect()
 
     # ACT
     y_one_hot = selector.one_hot_encode(y=y)
@@ -34,7 +34,7 @@ def test__scale_data():
     Test scale_data() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_event_bd20ms.XBOX2EventBD20msSelect()
     X = np.array([[[0, 0, 0], [1, 1, 1]]])
     X_expected = np.array([[[-1, -1, -1], [1, 1, 1]]])
 
@@ -55,7 +55,7 @@ def test__read_hdf_dataset(tmpdir, dataset_expected):
     Test read_hdf_dataset() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_event_bd20ms.XBOX2EventBD20msSelect()
     path = tmpdir.join("dummy.hdf")
     context_dummy = h5py.File(path, 'w')
     test_key = "test"
@@ -76,7 +76,7 @@ def test__read_hdf_dataset_error(tmpdir):
     Test read_hdf_dataset() function errors
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_event_bd20ms.XBOX2EventBD20msSelect()
     path = tmpdir.join("dummy.hdf")
     context_dummy = h5py.File(path, 'w')
     test_key = "test"
@@ -96,7 +96,7 @@ def test__select_trend_data_events():
     Test select_trend_data_events() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_event_bd20ms.XBOX2EventBD20msSelect()
     dummy_event_timestamps = np.array([np.datetime64('2021-08-18T17:59:00'),
                                        np.datetime64('2021-08-18T17:59:04'),
                                        np.datetime64('2021-08-18T17:59:02'),
@@ -111,8 +111,8 @@ def test__select_trend_data_events():
 
     # ACT
     time_filter_out = selector.select_trend_data_events(dummy_event_timestamps,
-                                                       dummy_trend_timestamps,
-                                                       time_threshold)
+                                                        dummy_trend_timestamps,
+                                                        time_threshold)
 
     # ASSERT
     assert (time_filter_expected == time_filter_out).all()
@@ -127,7 +127,7 @@ def test__select_events(tmpdir, dummy_features, selection_filter_expected):
     Test create_breakdown_selection_filter() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_event_bd20ms.XBOX2EventBD20msSelect()
     path = tmpdir.join("dummy.hdf")
     context_dummy = h5py.File(path, 'w')
     dummy_event_timestamps = np.array([np.datetime64('2021-08-18T17:59:00'),
@@ -139,6 +139,7 @@ def test__select_events(tmpdir, dummy_features, selection_filter_expected):
                                        np.datetime64('2021-08-18T17:59:02'),
                                        np.datetime64('2021-08-18T17:59:03')])
     dummy_is_healthy_labels = np.array([True, True, False, False])
+
     with context_dummy as f:
         f.create_dataset("is_bd_in_40ms", data=dummy_features)
         f.create_dataset("is_bd_in_20ms", data=dummy_features)
@@ -155,26 +156,34 @@ def test__select_events(tmpdir, dummy_features, selection_filter_expected):
     assert (selection_filter_expected == selection_filter_out).all()
 
 
-@pytest.mark.parametrize("data1, data2",
-                         [([10, 20], [30, 40]),
-                          ([-1., 3.], [20., 1.234])
+@pytest.mark.parametrize("dummy_data",
+                         [([10, 20]),
+                          ([-1., 3.])
                           ])
-def test__select_features(data1, data2):
+def test__select_features(dummy_data):
     """
     Test select_features() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_event_bd20ms.XBOX2EventBD20msSelect()
     d = {'Timestamp': [1, 2],
          'PrevTrendData__Timestamp': [3, 4],
          'is_bd': [5, 6],
          'is_healthy': [7, 8],
          'is_bd_in_20ms': [9, 10],
-         'is_bd_in_40ms': [11, 12],
-         'col1': data1,
-         'col2': data2}
+         'is_bd_in_40ms': [11, 12]
+         }
+    selection_list = ["DC_Down__D1", "DC_Down__D9", "DC_Down__tsfresh__mean", "DC_Down__tsfresh__maximum",
+                          "DC_Down__tsfresh__median", "DC_Down__tsfresh__minimum",
+                          "DC_Up__D1", "DC_Up__D9", "DC_Up__tsfresh__mean", "DC_Up__tsfresh__maximum",
+                          "DC_Up__tsfresh__median", "DC_Up__tsfresh__minimum",
+                          "PEI_Amplitude__pulse_length", "PEI_Amplitude__pulse_amplitude",
+                          "PKI_Amplitude__pulse_length", "PKI_Amplitude__pulse_amplitude",
+                          "PSI_Amplitude__pulse_length", "PSI_Amplitude__pulse_amplitude"]
+    for name in selection_list:
+        d[name] = dummy_data
     df = pd.DataFrame(data=d)
-    X_expected = df[pd.Index(['col1', 'col2'])].to_numpy(dtype=float)
+    X_expected = df[pd.Index(selection_list)].to_numpy(dtype=float)
     X_expected = np.nan_to_num(X_expected[..., np.newaxis])
 
     # ACT
@@ -185,22 +194,21 @@ def test__select_features(data1, data2):
 
 
 @pytest.mark.parametrize("data",
-                         [np.ones((10, ), dtype=bool),
-                          np.zeros((10, ), dtype=bool)
+                         [np.ones((10,), dtype=bool),
+                          np.zeros((10,), dtype=bool)
                           ])
 def test__select_labels(data):
     """
     Test load_X_data() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_event_bd20ms.XBOX2EventBD20msSelect()
     d = {'is_healthy': data}
     df = pd.DataFrame(data=d)
     y_expected = df['is_healthy'].to_numpy(dtype=float)
 
     # ACT
     y_out = selector.select_labels(df)
-
 
     # ASSERT
     assert (y_out == y_expected).all()
@@ -211,7 +219,7 @@ def test__load_dataset(tmpdir):
     Test load_dataset() function
     """
     # ARRANGE
-    simple_selector = simple_select.SimpleSelect()
+    selector = XBOX2_event_bd20ms.XBOX2EventBD20msSelect()
     path = tmpdir.join("context.hdf")
     context_dummy = h5py.File(path, 'w')
     dummy_is_bd_in_40ms_labels = np.ones((10,), dtype=bool)
@@ -244,8 +252,15 @@ def test__load_dataset(tmpdir):
         f.create_dataset("is_bd_in_40ms", data=dummy_is_bd_in_40ms_labels)
         f.create_dataset("is_bd_in_20ms", data=dummy_is_bd_in_20ms_labels)
         f.create_dataset("is_bd", data=dummy_is_bd_labels)
-        f.create_dataset("test_data1", data=4 * np.ones((10,)))
-        f.create_dataset("test_data2", data=np.zeros((10,)))
+        selection_list = ["DC_Down__D1", "DC_Down__D9", "DC_Down__tsfresh__mean", "DC_Down__tsfresh__maximum",
+                          "DC_Down__tsfresh__median", "DC_Down__tsfresh__minimum",
+                          "DC_Up__D1", "DC_Up__D9", "DC_Up__tsfresh__mean", "DC_Up__tsfresh__maximum",
+                          "DC_Up__tsfresh__median", "DC_Up__tsfresh__minimum",
+                          "PEI_Amplitude__pulse_length", "PEI_Amplitude__pulse_amplitude",
+                          "PKI_Amplitude__pulse_length", "PKI_Amplitude__pulse_amplitude",
+                          "PSI_Amplitude__pulse_length", "PSI_Amplitude__pulse_amplitude"]
+        for name in selection_list:
+            f.create_dataset(name, data=np.ones((10,)))
         f["Timestamp"] = dummy_event_timestamps.astype(h5py.opaque_dtype(dummy_event_timestamps.dtype))
         f["PrevTrendData/Timestamp"] = dummy_trend_timestamps.astype(h5py.opaque_dtype(dummy_trend_timestamps.dtype))
         f.create_dataset("clic_label/is_healthy", data=dummy_is_healthy_labels)
@@ -255,9 +270,9 @@ def test__load_dataset(tmpdir):
 
     # ACT
     np.random.seed(42)
-    train, valid, test = dataset_creator.load_dataset(creator=simple_selector, hdf_dir=tmpdir)
+    train, valid, test = dataset_creator.load_dataset(creator=selector, hdf_dir=tmpdir)
     sum_elements = len(train.idx) + len(valid.idx) + len(test.idx)
-    splits = (len(train.idx)/sum_elements, len(valid.idx)/sum_elements, len(test.idx)/sum_elements)
+    splits = (len(train.idx) / sum_elements, len(valid.idx) / sum_elements, len(test.idx) / sum_elements)
 
     # ASSERT
     assert splits == splits_expected
--- tests/unit/xbox2_specific/datasets/test_XBOX2_trend_bd20ms.py
+++ tests/unit/xbox2_specific/datasets/test_XBOX2_trend_bd20ms.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import pytest
 from src.utils import dataset_creator
-from src.xbox2_specific.datasets import simple_select
+from src.xbox2_specific.datasets import XBOX2_trend_bd20ms
 
 
 @pytest.mark.parametrize("y, \
@@ -20,7 +20,7 @@ def test__one_hot(y, y_one_hot_expected):
     Test one_hot function of dataset_creator
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_trend_bd20ms.XBOX2TrendBD20msSelect()
 
     # ACT
     y_one_hot = selector.one_hot_encode(y=y)
@@ -29,12 +29,13 @@ def test__one_hot(y, y_one_hot_expected):
     assert (y_one_hot == y_one_hot_expected).all()
 
 
+@pytest.mark.skip(reason='not finished')
 def test__scale_data():
     """
     Test scale_data() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_trend_bd20ms.XBOX2TrendBD20msSelect()
     X = np.array([[[0, 0, 0], [1, 1, 1]]])
     X_expected = np.array([[[-1, -1, -1], [1, 1, 1]]])
 
@@ -55,7 +56,7 @@ def test__read_hdf_dataset(tmpdir, dataset_expected):
     Test read_hdf_dataset() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_trend_bd20ms.XBOX2TrendBD20msSelect()
     path = tmpdir.join("dummy.hdf")
     context_dummy = h5py.File(path, 'w')
     test_key = "test"
@@ -76,7 +77,7 @@ def test__read_hdf_dataset_error(tmpdir):
     Test read_hdf_dataset() function errors
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_trend_bd20ms.XBOX2TrendBD20msSelect()
     path = tmpdir.join("dummy.hdf")
     context_dummy = h5py.File(path, 'w')
     test_key = "test"
@@ -96,7 +97,7 @@ def test__select_trend_data_events():
     Test select_trend_data_events() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_trend_bd20ms.XBOX2TrendBD20msSelect()
     dummy_event_timestamps = np.array([np.datetime64('2021-08-18T17:59:00'),
                                        np.datetime64('2021-08-18T17:59:04'),
                                        np.datetime64('2021-08-18T17:59:02'),
@@ -127,7 +128,7 @@ def test__select_events(tmpdir, dummy_features, selection_filter_expected):
     Test create_breakdown_selection_filter() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_trend_bd20ms.XBOX2TrendBD20msSelect()
     path = tmpdir.join("dummy.hdf")
     context_dummy = h5py.File(path, 'w')
     dummy_event_timestamps = np.array([np.datetime64('2021-08-18T17:59:00'),
@@ -155,26 +156,39 @@ def test__select_events(tmpdir, dummy_features, selection_filter_expected):
     assert (selection_filter_expected == selection_filter_out).all()
 
 
-@pytest.mark.parametrize("data1, data2",
-                         [([10, 20], [30, 40]),
-                          ([-1., 3.], [20., 1.234])
+@pytest.mark.parametrize("dummy_data",
+                         [([10, 20]),
+                          ([-1., 3.])
                           ])
-def test__select_features(data1, data2):
+def test__select_features(dummy_data):
     """
     Test select_features() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_trend_bd20ms.XBOX2TrendBD20msSelect()
     d = {'Timestamp': [1, 2],
          'PrevTrendData__Timestamp': [3, 4],
          'is_bd': [5, 6],
          'is_healthy': [7, 8],
          'is_bd_in_20ms': [9, 10],
-         'is_bd_in_40ms': [11, 12],
-         'col1': data1,
-         'col2': data2}
+         'is_bd_in_40ms': [11, 12]
+         }
+    selection_list = ["PrevTrendData__Loadside_win", "PrevTrendData__Tubeside_win",
+                          "PrevTrendData__Collector", "PrevTrendData__Gun", "PrevTrendData__IP_before_PC",
+                          "PrevTrendData__PC_IP", "PrevTrendData__WG_IP", "PrevTrendData__IP_Load",
+                          "PrevTrendData__IP_before_structure", "PrevTrendData__US_Beam_Axis_IP",
+                          "PrevTrendData__Klystron_Flange_Temp", "PrevTrendData__Load_Temp",
+                          "PrevTrendData__PC_Left_Cavity_Temp", "PrevTrendData__PC_Right_Cavity_Temp",
+                          "PrevTrendData__Bunker_WG_Temp", "PrevTrendData__Structure_Input_Temp",
+                          "PrevTrendData__Chiller_1", "PrevTrendData__Chiller_2", "PrevTrendData__Chiller_3",
+                          "PrevTrendData__PKI_FT_avg", "PrevTrendData__PSI_FT_avg", "PrevTrendData__PSR_FT_avg",
+                          "PrevTrendData__PSI_max", "PrevTrendData__PSR_max", "PrevTrendData__PEI_max",
+                          "PrevTrendData__DC_Down_min", "PrevTrendData__DC_Up_min",
+                          "PrevTrendData__PSI_Pulse_Width"]
+    for name in selection_list:
+        d[name] = dummy_data
     df = pd.DataFrame(data=d)
-    X_expected = df[pd.Index(['col1', 'col2'])].to_numpy(dtype=float)
+    X_expected = df[pd.Index(selection_list)].to_numpy(dtype=float)
     X_expected = np.nan_to_num(X_expected[..., np.newaxis])
 
     # ACT
@@ -184,6 +198,7 @@ def test__select_features(data1, data2):
     assert (X_out == X_expected).all()
 
 
+@pytest.mark.skip(reason='not finished')
 @pytest.mark.parametrize("data",
                          [np.ones((10, ), dtype=bool),
                           np.zeros((10, ), dtype=bool)
@@ -193,7 +208,7 @@ def test__select_labels(data):
     Test load_X_data() function
     """
     # ARRANGE
-    selector = simple_select.SimpleSelect()
+    selector = XBOX2_trend_bd20ms.XBOX2TrendBD20msSelect()
     d = {'is_healthy': data}
     df = pd.DataFrame(data=d)
     y_expected = df['is_healthy'].to_numpy(dtype=float)
@@ -206,12 +221,13 @@ def test__select_labels(data):
     assert (y_out == y_expected).all()
 
 
+@pytest.mark.skip(reason='not finished')
 def test__load_dataset(tmpdir):
     """
     Test load_dataset() function
     """
     # ARRANGE
-    simple_selector = simple_select.SimpleSelect()
+    selector = XBOX2_trend_bd20ms.XBOX2TrendBD20msSelect()
     path = tmpdir.join("context.hdf")
     context_dummy = h5py.File(path, 'w')
     dummy_is_bd_in_40ms_labels = np.ones((10,), dtype=bool)
@@ -244,8 +260,15 @@ def test__load_dataset(tmpdir):
         f.create_dataset("is_bd_in_40ms", data=dummy_is_bd_in_40ms_labels)
         f.create_dataset("is_bd_in_20ms", data=dummy_is_bd_in_20ms_labels)
         f.create_dataset("is_bd", data=dummy_is_bd_labels)
-        f.create_dataset("test_data1", data=4 * np.ones((10,)))
-        f.create_dataset("test_data2", data=np.zeros((10,)))
+        selection_list = ["DC_Down__D1", "DC_Down__D9", "DC_Down__tsfresh__mean", "DC_Down__tsfresh__maximum",
+                          "DC_Down__tsfresh__median", "DC_Down__tsfresh__minimum",
+                          "DC_Up__D1", "DC_Up__D9", "DC_Up__tsfresh__mean", "DC_Up__tsfresh__maximum",
+                          "DC_Up__tsfresh__median", "DC_Up__tsfresh__minimum",
+                          "PEI_Amplitude__pulse_length", "PEI_Amplitude__pulse_amplitude",
+                          "PKI_Amplitude__pulse_length", "PKI_Amplitude__pulse_amplitude",
+                          "PSI_Amplitude__pulse_length", "PSI_Amplitude__pulse_amplitude"]
+        for name in selection_list:
+            f.create_dataset(name, data=np.ones((10,)))
         f["Timestamp"] = dummy_event_timestamps.astype(h5py.opaque_dtype(dummy_event_timestamps.dtype))
         f["PrevTrendData/Timestamp"] = dummy_trend_timestamps.astype(h5py.opaque_dtype(dummy_trend_timestamps.dtype))
         f.create_dataset("clic_label/is_healthy", data=dummy_is_healthy_labels)
@@ -255,7 +278,7 @@ def test__load_dataset(tmpdir):
 
     # ACT
     np.random.seed(42)
-    train, valid, test = dataset_creator.load_dataset(creator=simple_selector, hdf_dir=tmpdir)
+    train, valid, test = dataset_creator.load_dataset(creator=selector, hdf_dir=tmpdir)
     sum_elements = len(train.idx) + len(valid.idx) + len(test.idx)
     splits = (len(train.idx)/sum_elements, len(valid.idx)/sum_elements, len(test.idx)/sum_elements)
 
