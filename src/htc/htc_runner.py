adba5eb12c6aaa740d4c2e0a42be5dd82a908e6f
cobermai <christoph.obermair@cern.ch Bro>
2021-08-24 14:59:16 +0200

    improved htc script

--- src/htc/htc_runner.py
+++ src/htc/htc_runner.py
@@ -46,10 +46,10 @@ class HTCondorRunner:
                 file.write("#!/bin/bash\n")
                 #file.write(f"cd {work_dir}\n")
                 file.write(f"source .{work_dir}/venv/bin/activate\n")
-                #file.write("ls ./venv/bin/\n")
-                file.write("pwd\n")
+                file.write("ls ./venv/bin/\n")
                 file.write("python3 -V\n")
-                file.write("pip3 list\n")
+                file.write(f"cd {work_dir}/venv/bin\n")
+                file.write("pwd\n")
                 file.write(f"python3 {work_dir / main_name} --file_path={work_dir} --output_path={output_dir}")
             except IOError as e:
                 print(f"I/O error({e.errno}): {e.strerror}")
