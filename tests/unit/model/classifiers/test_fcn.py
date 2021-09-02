0759e89152d5ce4a45dafed1c796af03a3c4168b
Holger Severin Bovbjerg <31927268+HolgerBovbjerg@users.noreply.github.com>
2021-09-02 11:29:58 +0200

    Add test for number of parameters in models

--- tests/unit/model/classifiers/test_fcn.py
+++ tests/unit/model/classifiers/test_fcn.py
@@ -9,6 +9,7 @@ def test__fcn():
     # ARRANGE
     layer_model_expected_names = ['cnn_block', 'cnn_block_1', 'cnn_block_2',
                                   'global_average_pooling1d', 'dense']
+    param_count_expected = 265986
 
     # ACT
     model_fcn = fcn.FCNBlock(2)
@@ -18,9 +19,13 @@ def test__fcn():
         layer_model_out_name = layer_model_out.name
         layer_model_out_names.append(layer_model_out_name)
 
+    model_fcn.build((None, 195, 1))
+    param_count = model_fcn.count_params()
+
     keras.backend.clear_session()
 
     # ASSERT
     assert layer_model_expected_names == layer_model_out_names
+    assert param_count == param_count_expected
 
 
--- tests/unit/model/classifiers/test_time_cnn.py
+++ tests/unit/model/classifiers/test_time_cnn.py
@@ -8,7 +8,8 @@ def test__time_cnn():
     """
     # ARRANGE
     layer_model_expected_names = ['conv1d', 'conv1d_1', 'average_pooling1d', 'conv1d_2',
-                                  'conv1d_3','average_pooling1d_1', 'flatten', 'dense']
+                                  'conv1d_3', 'average_pooling1d_1', 'flatten', 'dense']
+    param_count_expected = 1022
 
     # ACT
     model_time_cnn = time_cnn.TimeCNNBlock(2)
@@ -18,9 +19,12 @@ def test__time_cnn():
         layer_model_out_name = layer_model_out.name
         layer_model_out_names.append(layer_model_out_name)
 
+    model_time_cnn.build((None, 195, 1))
+    param_count = model_time_cnn.count_params()
+
     keras.backend.clear_session()
 
     # ASSERT
     assert layer_model_expected_names == layer_model_out_names
-
+    assert param_count == param_count_expected
 
