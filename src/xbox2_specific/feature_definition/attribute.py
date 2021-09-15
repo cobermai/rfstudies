c4801cd57f816c5e90c8dd233fd5c0a6685b7d77
Holger Severin Bovbjerg <31927268+HolgerBovbjerg@users.noreply.github.com>
2021-09-15 15:33:25 +0200

    Move run_no generation to feature post_processing

--- src/xbox2_specific/feature_definition/attribute.py
+++ src/xbox2_specific/feature_definition/attribute.py
@@ -29,15 +29,6 @@ def get_event_attribute_features(length: int) -> typing.Generator:
                                          "healthy or normal log signal, and 3 for a breakdown. The label 1 and 2 stood"
                                          "for breakdown in 20ms and 40ms, so the signals prior to a breakdown.")
 
-    yield EventAttributeFeature(name="run_no",
-                                func=_get_run_no,
-                                length=length,
-                                hdf_path="/",
-                                output_dtype=int,
-                                info="The run number of the based on manually defined stable run periods. "
-                                     "Run numbers 1-9 define stable runs and negative numbers denote commisioning"
-                                     "A 0 denotes data outside stable runs.")
-
 
 def _log_type_creator(type_of_interest: str) -> typing.Callable:
     """creates functions that return True if the input value matches the translation of the is_type label and
@@ -70,49 +61,3 @@ def _get_timestamp(attrs: h5py.AttributeManager):
     """
     datetime_str = attrs["Timestamp"][:-1]
     return np.datetime64(datetime_str).astype(h5py.opaque_dtype('M8[us]'))
-
-
-def assign_run_no(timestamp: np.datetime64):
-    # Hardcoded timestamps for classifying runs. Format is [start, stop, run_no]. Negative run_no is commissioning.
-    timestamp_list_run = np.array([
-        [np.datetime64('2018-05-15 21:16:59.626459'), np.datetime64('2018-05-23 14:41:58.024856'), -1],
-        [np.datetime64('2018-05-23 14:42:58.036909'), np.datetime64('2018-06-01 11:41:23.304752'), 1],
-        [np.datetime64('2018-06-01 11:42:23.326588'), np.datetime64('2018-06-10 09:57:46.167949'), -2],
-        [np.datetime64('2018-06-10 09:58:46.176918'), np.datetime64('2018-06-20 10:49:26.504749'), 2],
-        [np.datetime64('2018-06-20 10:50:26.517873'), np.datetime64('2018-06-27 01:56:14.100424'), -3],
-        [np.datetime64('2018-06-27 01:57:14.092206'), np.datetime64('2018-07-17 08:49:18.581234'), 3],
-        [np.datetime64('2018-07-17 08:50:18.644042'), np.datetime64('2018-07-17 14:24:22.808270'), -4],
-        [np.datetime64('2018-07-17 14:25:22.808983'), np.datetime64('2018-07-26 15:56:55.238768'), 4],
-        [np.datetime64('2018-07-26 15:57:55.254684'), np.datetime64('2018-07-30 12:58:28.800693'), -5],
-        [np.datetime64('2018-07-30 12:59:28.850502'), np.datetime64('2018-08-09 07:18:19.717621'), 5],
-        [np.datetime64('2018-08-09 07:19:19.717776'), np.datetime64('2018-08-16 07:48:45.260491'), -6],
-        [np.datetime64('2018-08-16 07:49:45.217265'), np.datetime64('2018-08-22 19:07:06.581874'), 6],
-        [np.datetime64('2018-08-24 22:53:03.560161'), np.datetime64('2018-08-27 20:21:22.319445'), -7],
-        [np.datetime64('2018-08-27 20:22:22.331644'), np.datetime64('2018-09-03 09:53:18.547360'), 7],
-        [np.datetime64('2018-09-03 09:54:18.540067'), np.datetime64('2018-09-05 16:48:36.589576'), -8],
-        [np.datetime64('2018-09-05 16:49:36.595947'), np.datetime64('2018-09-17 06:27:33.398326'), 8],
-        [np.datetime64('2018-09-17 06:28:33.412608'), np.datetime64('2018-09-19 00:05:14.894480'), -9],
-        [np.datetime64('2018-09-19 00:06:14.912150'), np.datetime64('2018-09-25 09:51:59.222968'), 9]
-    ])
-
-    run_no_assigned = 0
-    for run in timestamp_list_run:
-        run_start = run[0]
-        run_end = run[1]
-        if (timestamp >= run_start) & (timestamp <= run_end):
-            run_no_assigned = run[2]
-            break
-
-    return run_no_assigned
-
-
-def _get_run_no(attrs: h5py.AttributeManager):
-    """
-    returns the Timestamp from group properties/attribute in numpy datetime format
-    :param attrs: the h5py.AttributeManager of an hdf.Group object
-    :return: numpy datetime format of the timestamp
-    """
-    datetime_str = attrs["Timestamp"][:-1]
-    timestamp = np.datetime64(datetime_str)
-    run_no = assign_run_no(timestamp)
-    return run_no
