89c844aa39f161433fc0cb624ae66c5ab84e5442
Holger Severin Bovbjerg <31927268+HolgerBovbjerg@users.noreply.github.com>
2021-12-08 17:25:12 +0100

    Move __init__ includes of XBOX2 datasets to XBOX2 specific datasets

--- src/datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.xbox2_specific.datasets.XBOX2_event_all_bd_20ms import XBOX2EventAllBD20msSelect
-from src.xbox2_specific.datasets.XBOX2_event_primo_bd_20ms import XBOX2EventPrimoBD20msSelect
-from src.xbox2_specific.datasets.XBOX2_event_followup_bd_20ms import XBOX2EventFollowupBD20msSelect
-from src.xbox2_specific.datasets.XBOX2_trend_all_bd_20ms import XBOX2TrendAllBD20msSelect
-from src.xbox2_specific.datasets.XBOX2_trend_primo_bd_20ms import XBOX2TrendPrimoBD20msSelect
-from src.xbox2_specific.datasets.XBOX2_trend_followup_bd_20ms import XBOX2TrendFollowupBD20msSelect
